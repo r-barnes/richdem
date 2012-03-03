34819805347489755dcb56a7dc1aa645dba34f54
Richard Barnes <rbarnes@umn.edu>
2012-03-03 03:12:07 +0000

    `colour` option added to `print_dem` const qualifier added to `print_flow`
    git-svn-id: file:///home/rick/.svn/richdem@15 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- utility.cpp
+++ utility.cpp
@@ -2,19 +2,19 @@
 #include "data_structures.h"
 #include <cstdio>
 
-void print_dem(float_2d &elevations, int mark_x=-1, int mark_y=-1){
+void print_dem(const float_2d &elevations, int mark_x, int mark_y, int colour){
 	for(int y=0;y<elevations.height();y++){
 		for(int x=0;x<elevations.width();x++){
-			if(x==mark_x && y==mark_y) printf("\033[91m");
+			if(x!=-1 && x==mark_x && y==mark_y) printf("\033[%dm",colour);
 			printf("%.0f ",elevations(x,y));
-			if(x==mark_x && y==mark_y) printf("\033[39m");
+			if(x!=-1 && x==mark_x && y==mark_y) printf("\033[39m");
 		}
 		printf("\n");
 	}
 	printf("\n");
 }
 
-void print_flow(float_2d &flowdirs){
+void print_flow(const float_2d &flowdirs){
 	for(int y=0;y<flowdirs.height();y++){
 		for(int x=0;x<flowdirs.width();x++)
 			printf("%f ",flowdirs(x,y));
