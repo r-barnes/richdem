5829f868643f5864ae1e441731ad03426347abda
Richard Barnes <rbarnes@umn.edu>
2012-03-09 03:51:24 +0000

    Added INTERIOR_GRID macro
    git-svn-id: file:///home/rick/.svn/richdem@32 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- utility.h
+++ utility.h
@@ -14,6 +14,7 @@
 //432
 
 #define IN_GRID(x,y,x_max,y_max) (x>=0 && y>=0 && x<x_max && y<y_max)
+#define INTERIOR_GRID(x,y,x_max,y_max) (x>=1 && y>=1 && x<x_max-1 && y<y_max-1)
 #define EDGE_GRID(x,y,x_max,y_max) (x==0 || y==0 || x==x_max-1 || y==y_max-1)
 #define d8_NO_DATA		-100
 #define dinf_NO_DATA	-9999
@@ -33,7 +34,15 @@ const std::string fd[9]={"·","←","↖","↑","↗","→","↘","↓","↙"};
 //876
 
 void print_dem(const float_2d &elevations, int mark_x=-1, int mark_y=-1, int colour=91);
-void print_flow(const float_2d &flowdirs);
-void print_char_2d(const char_2d &arr);
+
+template <class T>
+void print2d(const char f[], const array2d<T> &arr){
+	for(int y=0;y<arr.height();y++){
+		for(int x=0;x<arr.width();x++)
+			printf(f,arr(x,y));
+		printf("\n");
+	}
+	printf("\n");
+}
 
 #endif
