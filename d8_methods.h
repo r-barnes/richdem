e5017cb381453041bfd231f704eba1c4eb96c524
Richard Barnes <rbarnes@umn.edu>
2012-03-13 18:41:21 +0000

    Added d8_slope function
    git-svn-id: file:///home/rick/.svn/richdem@56 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- d8_methods.h
+++ d8_methods.h
@@ -3,7 +3,13 @@
 
 #include "data_structures.h"
 
+#define SLOPE_RISERUN	1
+#define SLOPE_PERCENT	2
+#define SLOPE_RADIAN	3
+#define SLOPE_DEGREE	4
+
 int d8_flow_directions(const float_2d &elevations, char_2d &flowdirs, bool init=true);
 int d8_upslope_area(const char_2d &flowdirs, uint_2d &area);
+int d8_slope(const float_2d &elevations, float_2d &slopes, int slope_type=SLOPE_RISERUN);
 
 #endif
