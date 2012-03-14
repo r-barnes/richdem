cd3a2dec9a2082d7b404e327af925de845a7e6c9
Richard Barnes <rbarnes@umn.edu>
2012-03-14 18:58:25 +0000

    Corrected problem with making timediff universal (needed to include sys/time.h)
    git-svn-id: file:///home/rick/.svn/richdem@61 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- interface.h
+++ interface.h
@@ -1,4 +1,5 @@
-#include<cstdio>
+#include <cstdio>
+#include <sys/time.h>
 #define diagnostic_arg(message,...) fprintf(stderr,message,__VA_ARGS__)
 #define diagnostic(message) fprintf(stderr,message)
 double progress_bar(int percent);
