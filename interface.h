1084e7e226f665e2a8afcaa3337cc32c3897fe11
Richard Barnes <rbarnes@umn.edu>
2012-03-14 18:56:25 +0000

    Made timediff a global function
    git-svn-id: file:///home/rick/.svn/richdem@60 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- interface.h
+++ interface.h
@@ -2,3 +2,4 @@
 #define diagnostic_arg(message,...) fprintf(stderr,message,__VA_ARGS__)
 #define diagnostic(message) fprintf(stderr,message)
 double progress_bar(int percent);
+double timediff(const timeval &startTime);
