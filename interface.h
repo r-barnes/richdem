0dc56ba59e002e2566c5a63cfeb40396bfa34696
Richard Barnes <rbarnes@umn.edu>
2012-03-17 21:59:28 +0000

    Added double-define guards to interface.h
    git-svn-id: file:///home/rick/.svn/richdem@71 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- interface.h
+++ interface.h
@@ -1,6 +1,11 @@
+#ifndef _interface_included
+#define _interface_included
+
 #include <cstdio>
 #include <sys/time.h>
 #define diagnostic_arg(message,...) fprintf(stderr,message,__VA_ARGS__)
 #define diagnostic(message) fprintf(stderr,message)
 double progress_bar(int percent);
 double timediff(const timeval &startTime);
+
+#endif
