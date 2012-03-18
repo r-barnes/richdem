3966c2d53080a4e5286222ed5cfeb5e7517c27c7
Richard Barnes <rbarnes@umn.edu>
2012-03-18 23:15:53 +0000

    Added a function to difference array2d structures
    git-svn-id: file:///home/rick/.svn/richdem@81 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- debug.h
+++ debug.h
@@ -3,4 +3,14 @@
 
 void print_edges(float_2d &elevations, std::deque<grid_cell> &low_edges, std::deque<grid_cell> &high_edges);
 
+template <class T> //TODO: Needs error checking for dimensions, et cetera
+void array2ddiff(const array2d<T> &arr1, const array2d<T> &arr2, array2d<T> &result){
+	for(int x=0;x<arr1.width();x++)
+	for(int y=0;y<arr2.height();y++)
+		if(arr1(x,y)==arr1.no_data || arr2(x,y)==arr2.no_data)
+			result(x,y)=arr1.no_data;
+		else
+			result(x,y)=arr1(x,y)-arr2(x,y);
+}
+
 #endif
