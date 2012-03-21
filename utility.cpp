34a7700c86d2fbc7a44b16c561647aea53b5ea47
Richard Barnes <rbarnes@umn.edu>
2012-03-21 00:09:30 +0000

    Added operator>> for dealing with must_be structs. Useful for dealing with file input.
    git-svn-id: file:///home/rick/.svn/richdem@91 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- utility.cpp
+++ utility.cpp
@@ -1,4 +1,5 @@
 #include "utility.h"
+#include "interface.h"
 #include "data_structures.h"
 #include <cstdio>
 
@@ -12,4 +13,14 @@ void print_dem(const float_2d &elevations, int mark_x, int mark_y, int colour){
 		printf("\n");
 	}
 	printf("\n");
+}
+
+std::istream& operator>>(std::istream& inputstream, const must_be &a){
+    std::string in_val;
+    inputstream >> in_val;
+	if(in_val!=a.val){
+		diagnostic_arg("failed!\n\tCould not match \"%s\"!\n",a.val.c_str());
+		throw std::string("Input file didn't match expectations!");
+	}
+    return inputstream;
 }
