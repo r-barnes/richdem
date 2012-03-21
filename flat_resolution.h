d1544ce8c5aa52740b8f3aacdcc1a3257be1cdf6
Richard Barnes <rbarnes@umn.edu>
2012-03-21 21:06:16 +0000

    resolve_flats now takes an explicit argument for a returned mask array, for use with the flow directions functions
    git-svn-id: file:///home/rick/.svn/richdem@94 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- flat_resolution.h
+++ flat_resolution.h
@@ -3,6 +3,6 @@
 
 #include "data_structures.h"
 
-int resolve_flats(float_2d &elevations, const char_2d &flowdirs);
+int resolve_flats(const float_2d &elevations, const char_2d &flowdirs, int_2d &flat_resolution_mask);
 
 #endif
