f3e799bf482e16019d210b32e4817b20deb50f6c
Richard Barnes <rbarnes@umn.edu>
2012-03-22 19:14:54 +0000

    Should be set up to work with masking and on multiple data types, making it suitable for integration with resolve_flats()
    git-svn-id: file:///home/rick/.svn/richdem@106 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- dinf_methods.cpp
+++ dinf_methods.cpp
@@ -6,109 +6,6 @@
 #include <cmath>
 #include <queue>
 
-#define NO_DINF_FLOW -1
-
-/*
-814 citations
-@article{tarboton1997new,
-  title={A new method for the determination of flow directions and upslope areas in grid digital elevation models},
-  author={Tarboton, D.G.},
-  journal={Water resources research},
-  volume={33},
-  pages={309--319},
-  year={1997},
-  publisher={Citeseer}
-}
-*/
-
-
-//Table 1 of Tarboton
-int dy_e1[8]={0,-1,-1,0,0,1,1,0};
-int dx_e1[8]={1,0,0,-1,-1,0,0,1};
-int dy_e2[8]={-1,-1,-1,-1,1,1,1,1};
-int dx_e2[8]={1,1,-1,-1,-1,-1,1,1};
-double ac[8]={0,1,1,2,2,3,3,4};
-double af[8]={1,-1,1,-1,1,-1,1,-1};
-
-float dinf_FlowDir(const float_2d &elevations, const int x, const int y){
-	double smax=0;
-	int nmax=-1;
-	double rmax=0;
-
-	double e0,e1,e2,d1,d2,s1,s2,r,s;
-
-	if (elevations(x,y)==elevations.no_data) return dinf_NO_DATA; //Missing data
-	if (EDGE_GRID(x,y,elevations.width(),elevations.height())) return -1; //Edge cells do not have a defined flow direction
-
-	for(int n=0;n<8;n++){
-		if(!IN_GRID(x+dx_e1[n],y+dy_e1[n],elevations.width(),elevations.height())) continue;
-		if(!IN_GRID(x+dx_e2[n],y+dy_e2[n],elevations.width(),elevations.height())) continue;
-		if(elevations(x+dx_e1[n],y+dy_e1[n])==elevations.no_data) continue;
-		if(elevations(x+dx_e2[n],y+dy_e2[n])==elevations.no_data) continue;
-
-		e0=elevations(x,y);
-		e1=elevations(x+dx_e1[n],y+dy_e1[n]);
-		e2=elevations(x+dx_e2[n],y+dy_e2[n]);
-		d1=1;
-		d2=1;
-		s1=(e0-e1)/d1;
-		s2=(e1-e2)/d2;
-		r=atan2(s2,s1);
-		s=sqrt(s1*s1+s2*s2);
-		if(r<0){
-			r=0;
-			s=s1;
-		} else if(r>atan2(d2,d1)){
-			r=atan2(d2,d1);
-			s=(e0-e2)/sqrt(d1*d1+d2*d2);
-		}
-		if(s>smax){
-			smax=s;
-			nmax=n;
-			rmax=r;
-		}
-	}
-
-	double rg=NO_DINF_FLOW;
-	if(nmax!=-1)
-		rg=(af[nmax]*rmax+ac[nmax]*M_PI/2);
-
-	return rg;
-}
-
-void dinf_flow_directions(const float_2d &elevations, float_2d &flowdirs, bool init){
-	diagnostic_arg("The Dinf flow directions will require approximately %ldMB of RAM.\n",elevations.width()*elevations.height()*sizeof(float)/1024/1024);
-	diagnostic("Resizing flow directions matrix...");
-	flowdirs.resize(elevations.width(),elevations.height(),!init);
-	diagnostic("succeeded.\n");
-
-	if(init){
-		diagnostic("Setting no_data value on flowdirs matrix...");
-		flowdirs.no_data=dinf_NO_DATA;
-		diagnostic("succeeded.\n");
-
-		diagnostic("Initializing Dinf flow directions...\n");
-		flowdirs.init(NO_FLOW);
-		diagnostic("\tsucceeded.\n");
-	}
-
-	diagnostic("Calculating Dinf flow directions...\n");
-	progress_bar(-1);
-	#pragma omp parallel for
-	for(int x=0;x<elevations.width();x++){
-		progress_bar(x*omp_get_num_threads()*elevations.height()*100/(elevations.width()*elevations.height()));
-		for(int y=0;y<elevations.height();y++)
-			if(flowdirs(x,y)==NO_FLOW)
-				flowdirs(x,y)=dinf_FlowDir(elevations,x,y);
-	}
-	progress_bar(-1);
-	diagnostic("\tsucceeded.\n");
-}
-
-
-
-
-
 /*
 @inproceedings{Wallis2009,
 address = {Las Vegas, Nevada, USA},
@@ -146,11 +43,11 @@ To convert Dinf to this, take
       7                5,6
 */
 //These arrays have a 9th element which repeats the 8th element because floating point rounding errors occassionally result in the 9th element being accessed.
-int dinf_to_d8_low[9]= {4,3,2,1,1,7,6,5,5};
-int dinf_to_d8_high[9]={5,4,3,2,8,8,7,6,6};
+const int dinf_to_d8_low[9]= {4,3,2,1,1,7,6,5,5};
+const int dinf_to_d8_high[9]={5,4,3,2,8,8,7,6,6};
 
 bool does_cell_flow_into_me(float n_flowdir, int n){
-	if(n_flowdir==NO_DINF_FLOW || n_flowdir==dinf_NO_DATA) return false;
+	if(n_flowdir==NO_FLOW || n_flowdir==dinf_NO_DATA) return false;
 	n=inverse_flow[n];
 	int flown=(int)(n_flowdir/(M_PI/4));
 	return (dinf_to_d8_low[flown]==n || dinf_to_d8_high[flown]==n);
@@ -241,7 +138,7 @@ void dinf_upslope_area(const float_2d &flowdirs, float_2d &area){
 			if(does_cell_flow_into_me(flowdirs(c->x+dx[n],c->y+dy[n]),n))
 				area(c->x,c->y)+=proportion_i_get(flowdirs(c->x+dx[n],c->y+dy[n]),n)*area(c->x+dx[n],c->y+dy[n]);
 		}
-		if(flowdirs(c->x,c->y)!=dinf_NO_DATA && flowdirs(c->x,c->y)!=NO_DINF_FLOW){
+		if(flowdirs(c->x,c->y)!=dinf_NO_DATA && flowdirs(c->x,c->y)!=NO_FLOW){
 			int n_low= dinf_to_d8_low [(int)(flowdirs(c->x,c->y)/(M_PI/4))];
 			int n_high=dinf_to_d8_high[(int)(flowdirs(c->x,c->y)/(M_PI/4))];
 			if( (--dependency(c->x+dx[n_low],c->y+dy[n_low]))==0)
--- dinf_methods.h
+++ dinf_methods.h
@@ -1,7 +1,130 @@
 #ifndef _dinf_methods_included
 #define _dinf_methods_included
 
-void dinf_flow_directions(const float_2d &elevations, float_2d &flowdirs, bool init=true);
+#include "interface.h"
+#include "data_structures.h"
+#include <omp.h>
+#include "utility.h"
+
 void dinf_upslope_area(const float_2d &flowdirs, float_2d &area);
+
+/*
+814 citations
+@article{tarboton1997new,
+  title={A new method for the determination of flow directions and upslope areas in grid digital elevation models},
+  author={Tarboton, D.G.},
+  journal={Water resources research},
+  volume={33},
+  pages={309--319},
+  year={1997},
+  publisher={Citeseer}
+}
+*/
+
+//Table 1 of Tarboton
+static const int dy_e1[8]={0,-1,-1,0,0,1,1,0};
+static const int dx_e1[8]={1,0,0,-1,-1,0,0,1};
+static const int dy_e2[8]={-1,-1,-1,-1,1,1,1,1};
+static const int dx_e2[8]={1,1,-1,-1,-1,-1,1,1};
+static const double ac[8]={0,1,1,2,2,3,3,4};
+static const double af[8]={1,-1,1,-1,1,-1,1,-1};
+
+template <class T>
+float dinf_FlowDir(const array2d<T> &elevations, const int x, const int y){
+	double smax=0;
+	int nmax=-1;
+	double rmax=0;
+
+	double e0,e1,e2,d1,d2,s1,s2,r,s;
+
+	if (EDGE_GRID(x,y,elevations.width(),elevations.height())){
+		if(x==0 && y==0)
+			return 3*M_PI/4;	//D8: 2
+		else if(x==0 && y==elevations.height()-1)
+			return 5*M_PI/4;	//D8: 8
+		else if(x==elevations.width()-1 && y==0)
+			return 1*M_PI/4;	//D8: 4
+		else if(x==elevations.width()-1 && y==elevations.height()-1)
+			return 7*M_PI/4;	//D8: 6
+		else if(x==0)
+			return 4*M_PI/4;	//D8: 1
+		else if(x==elevations.width()-1)
+			return 0*M_PI/4;	//D8: 5
+		else if(y==0)
+			return 2*M_PI/4;	//D8: 3
+		else if(y==elevations.height()-1)
+			return 6*M_PI/4;	//D8: 7
+	}
+	
+	//Since I am not on the edge of the grid if I've made it this far, may neighbours cannot be off the grid
+	//Very negative no_data's should be acceptable, and suck water of the grid.
+	for(int n=0;n<8;n++){
+		//if(elevations(x+dx_e1[n],y+dy_e1[n])==elevations.no_data) continue;
+		//if(elevations(x+dx_e2[n],y+dy_e2[n])==elevations.no_data) continue;
+
+		e0=elevations(x,y);
+		e1=elevations(x+dx_e1[n],y+dy_e1[n]);
+		e2=elevations(x+dx_e2[n],y+dy_e2[n]);
+		d1=1;
+		d2=1;
+		s1=(e0-e1)/d1;
+		s2=(e1-e2)/d2;
+		r=atan2(s2,s1);
+		s=sqrt(s1*s1+s2*s2);
+		if(r<0){
+			r=0;
+			s=s1;
+		} else if(r>atan2(d2,d1)){
+			r=atan2(d2,d1);
+			s=(e0-e2)/sqrt(d1*d1+d2*d2);
+		}
+		if(s>smax){
+			smax=s;
+			nmax=n;
+			rmax=r;
+		}
+	}
+
+	double rg=NO_FLOW;
+	if(nmax!=-1)
+		rg=(af[nmax]*rmax+ac[nmax]*M_PI/2);
+
+	return rg;
+}
+
+template <class T>
+void dinf_flow_directions(const array2d<T> &elevations, float_2d &flowdirs, bool init=true){
+	diagnostic_arg("The Dinf flow directions will require approximately %ldMB of RAM.\n",elevations.width()*elevations.height()*sizeof(float)/1024/1024);
+	diagnostic("Resizing flow directions matrix...");
+	flowdirs.resize(elevations.width(),elevations.height(),!init);
+	diagnostic("succeeded.\n");
+
+	if(init){
+		diagnostic("Setting no_data value on flowdirs matrix...");
+		flowdirs.no_data=dinf_NO_DATA;
+		diagnostic("succeeded.\n");
+
+		diagnostic("Initializing Dinf flow directions...\n");
+		flowdirs.init(NO_FLOW);
+		diagnostic("\tsucceeded.\n");
+	}
+
+	diagnostic("Calculating Dinf flow directions...\n");
+	progress_bar(-1);
+	#pragma omp parallel for
+	for(int x=0;x<elevations.width();x++){
+		progress_bar(x*omp_get_num_threads()*elevations.height()*100/(elevations.width()*elevations.height()));
+		for(int y=0;y<elevations.height();y++)
+			if(elevations(x,y)==elevations.no_data){
+				if(init)
+					flowdirs(x,y)=dinf_NO_DATA;
+				else
+					continue;
+				} else if(flowdirs(x,y)==NO_FLOW)
+					flowdirs(x,y)=dinf_FlowDir(elevations,x,y);
+	}
+	progress_bar(-1);
+	diagnostic("\tsucceeded.\n");
+}
 
 #endif
