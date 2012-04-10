c46f2b8b863ef255c37947ba7757a4d7fb9364c6
Richard Barnes <rbarnes@umn.edu>
2012-04-10 01:10:52 +0000

    Changes
    git-svn-id: file:///home/rick/.svn/richdem@161 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- pit_fill.h
+++ pit_fill.h
@@ -1,7 +1,11 @@
 void pit_fill_planchon_direct(float_2d &elevations, float epsilon_straight, float epsilon_diagonal);
 void pit_fill_wang(float_2d &elevations);
 
+void pit_fill_wangND(float_2d &elevations);
+
 
 void pit_fill_barnes1(float_2d &elevations);
+void pit_fill_barnes1b(float_2d &elevations);
+
 void pit_fill_barnes2(float_2d &elevations);
 void pit_fill_barnes3(float_2d &elevations);
