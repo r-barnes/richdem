038f704fe360aaca16ef6ef0d0c2881b08571210
Richard Barnes <rbarnes@umn.edu>
2012-04-17 20:57:32 +0000

    Copied pit_fill.cpp into pit_fill_test.cpp. Culled pit_fill.cpp and pit_fill.h down into nice code blocks
    git-svn-id: file:///home/rick/.svn/richdem@172 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- pit_fill.h
+++ pit_fill.h
@@ -1,17 +1,1 @@
-void pit_fill_planchon_direct(float_2d &elevations, float epsilon_straight, float epsilon_diagonal);
-
-void pit_fill_planchon_optimized(float_2d &elevations, float epsilon);
-
-void pit_fill_wang(float_2d &elevations);
-
-void pit_fill_wangND(float_2d &elevations);
-
-
-void pit_fill_barnes1(float_2d &elevations);
-void pit_fill_barnes1b(float_2d &elevations);
-
-void pit_fill_barnes2(float_2d &elevations);
-void pit_fill_barnes3(float_2d &elevations);
-void pit_fill_barnes4(float_2d &elevations);
-void pit_fill_barnes5(float_2d &elevations);
-void pit_fill_barnes6(float_2d &elevations);
+void barnes_flood(float_2d &elevations);
