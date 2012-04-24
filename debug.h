bdf891f66d25b50a18e4c1510075596c7a1ed96e
Richard Barnes <rbarnes@umn.edu>
2012-04-24 21:26:03 +0000

    Improved "ddiff" function and added "dadiff" function for subtraction among angles.
    git-svn-id: file:///home/rick/.svn/richdem@181 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- debug.h
+++ debug.h
@@ -2,19 +2,51 @@
 #define _debug_included
 
 #include <deque>
+#include <cmath>
 
 #define PRINT(ARR,PREC,WIDTH) std::cout<<std::setprecision(PREC)<<std::setw(WIDTH)<<ARR<<std::endl;
 
 void print_edges(const float_2d &elevations, const std::deque<grid_cell> &low_edges, const std::deque<grid_cell> &high_edges);
 
 template <class T> //TODO: Needs error checking for dimensions, et cetera
-void array2ddiff(const array2d<T> &arr1, const array2d<T> &arr2, array2d<T> &result){
+void ddiff(const array2d<T> &arr1, const array2d<T> &arr2, array2d<T> &result){
+	diagnostic("Differencing the two arrays...\n");
+	if(arr1.width()!=arr2.width() || arr1.height()!=arr2.height())
+		diagnostic("failed! The arrays do not have the same dimensions!\n");
+	result.copyprops(arr1);
+	//Todo: It isn't guaranteed that subtraction of two elements will avoid the no_data value
+	#pragma omp parallel for
 	for(int x=0;x<arr1.width();x++)
 	for(int y=0;y<arr2.height();y++)
 		if(arr1(x,y)==arr1.no_data || arr2(x,y)==arr2.no_data)
 			result(x,y)=arr1.no_data;
 		else
-			result(x,y)=arr1(x,y)-arr2(x,y);
+			result(x,y)=fabs(arr1(x,y)-arr2(x,y));
+	diagnostic("success!\n");
+}
+
+template <class T> //TODO: Needs error checking for dimensions, et cetera
+void dadiff(const array2d<T> &arr1, const array2d<T> &arr2, array2d<T> &result){
+	diagnostic("Differencing the two arrays...\n");
+	if(arr1.width()!=arr2.width() || arr1.height()!=arr2.height())
+		diagnostic("failed! The arrays do not have the same dimensions!\n");
+	result.copyprops(arr1);
+	//Todo: It isn't guaranteed that subtraction of two elements will avoid the no_data value
+	#pragma omp parallel for
+	for(int x=0;x<arr1.width();x++)
+	for(int y=0;y<arr2.height();y++)
+		if(arr1(x,y)==arr1.no_data || arr2(x,y)==arr2.no_data)
+			result(x,y)=arr1.no_data;
+		else{
+			result(x,y)=fabs(arr1(x,y)-arr2(x,y));
+			if(result(x,y)>2*M_PI)
+				result(x,y)-=2*M_PI;
+			else if (result(x,y)>M_PI)
+				result(x,y)-=M_PI;
+//			if(result(x,y)>0.17453292519943295769)
+//				diagnostic_arg("%f-%f=%f\n",arr1(x,y),arr2(x,y),result(x,y));
+		}
+	diagnostic("success!\n");
 }
 
 
