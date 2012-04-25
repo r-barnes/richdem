288263180fff2107f06ee321a87e5f5d4ac4afad
Richard Barnes <rbarnes@umn.edu>
2012-04-25 16:42:21 +0000

    Added macro function for find dinf flow directions in a pit-filled DEM
    git-svn-id: file:///home/rick/.svn/richdem@187 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- debug.cpp
+++ debug.cpp
@@ -1,6 +1,8 @@
 #include <deque>
 #include "data_structures.h"
 #include "interface.h"
+#include "flat_resolution.h"
+#include "dinf_methods.h"
 
 void print_edges(const float_2d &elevations, const std::deque<grid_cell> &low_edges, const std::deque<grid_cell> &high_edges){
 	for(int y=0;y<elevations.height();y++){
@@ -19,4 +21,16 @@ void print_edges(const float_2d &elevations, const std::deque<grid_cell> &low_ed
 		}
 		diagnostic("\n");
 	}
+}
+
+
+
+void dinf_pit_flows(const float_2d &elevations, float_2d &flowdirs){
+	flowdirs.copyprops(elevations);
+	dinf_flow_directions(elevations,flowdirs);
+
+	int_2d flat_resolution_mask(elevations), groups;
+	resolve_flats_barnes(elevations,flowdirs,flat_resolution_mask,groups);
+
+	dinf_flow_flats(flat_resolution_mask,groups,flowdirs);
 }
