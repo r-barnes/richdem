74237a79821cf131e3b4978042bc7c0179a3e315
Richard Barnes <rbarnes@umn.edu>
2012-04-25 16:43:28 +0000

    Added another few functions from pit_fill_test.cpp
    git-svn-id: file:///home/rick/.svn/richdem@190 c4c8cafc-911d-468f-9f86-3f4f8cd09a06
    
    Former-commit-id: 9de1da383ec1d244e47bb544eaab199b4eaa1016

--- pit_fill.h
+++ pit_fill.h
@@ -1,1 +1,9 @@
+#ifndef pit_fill_include
+#define pit_fill_include
+#include "data_structures.h"
 void barnes_flood(float_2d &elevations);
+
+
+void pit_fill_barneslehman(float_2d &elevations);
+
+#endif
