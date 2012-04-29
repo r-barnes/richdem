8f7a0fe9b842bdd1acf58dfc93b038ed99d19b05
Richard Barnes <rbarnes@umn.edu>
2012-04-29 17:39:11 +0000

    Added the ROUND macro
    git-svn-id: file:///home/rick/.svn/richdem@197 c4c8cafc-911d-468f-9f86-3f4f8cd09a06
    
    Former-commit-id: fd6564baacba639583fbcad27f17a605fb31fa88

--- utility.h
+++ utility.h
@@ -25,8 +25,10 @@
 #define MAX(A,B) 	(((A)>(B))?(A):(B))
 #define DEG(A)		((A)*180.0/M_PI)
 
+#define ROUND(A)	floor((A) + 0.5)
+
 //D8 Directions
-const int dx[9]={0,-1,-1,0,1,1,1,0,-1};
+const int dx[9]={0,-1,-1,0,1,1,1,0,-1};	//TODO: These should be merged with my new dinf_d8 to reflect a uniform and intelligent directional system
 const int dy[9]={0,0,-1,-1,-1,0,1,1,1};
 const int dr[9]={0,1,SQRT2,1,SQRT2,1,SQRT2,1,SQRT2};
 const int inverse_flow[9]={0,5,6,7,8,1,2,3,4}; //Inverse of a given n from chart below
@@ -38,6 +40,7 @@ const std::string fd[9]={"·","←","↖","↑","↗","→","↘","↓","↙"};
 
 void print_dem(const float_2d &elevations, int mark_x=-1, int mark_y=-1, int colour=91);
 
+//TODO: Don't think I'm using the must_be functionality any more
 //Used for checking input from files for structured tags
 struct must_be{
 	std::string val;
