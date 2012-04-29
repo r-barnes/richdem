786822c2007fddf5238fbf5e214d33caeba6ccef
Richard Barnes <rbarnes@umn.edu>
2012-04-29 17:41:06 +0000

    In essence, made it so the ascii_output_data() procedure can handle multiple file types (at this point only AAIGrid and OMG)
    git-svn-id: file:///home/rick/.svn/richdem@198 c4c8cafc-911d-468f-9f86-3f4f8cd09a06
    
    Former-commit-id: 6083a459f99a0874bd97c3b4d32fd866f21bee5a

--- data_io.h
+++ data_io.h
@@ -3,18 +3,23 @@
 
 #include "interface.h"
 #include <fstream>
+#include <string>
 //#include <fcntl.h> //Used for posix_fallocate
 
 int load_ascii_data(const char filename[], float_2d &elevations);
 
+#define OUTPUT_DEM	1
+#define OUTPUT_OMG	2
 
 
 template <class T>
-int output_ascii_data(const char filename[], const array2d<T> &output_grid, int precision=3){
+int output_ascii_data(const std::string filename, const array2d<T> &output_grid, int precision=3){
 	std::ofstream fout;
+	std::string outputsep=" ";
+	int output_type=OUTPUT_DEM;
 
-	diagnostic_arg("Opening ASCII output file \"%s\"...",filename);
-	fout.open(filename);
+	diagnostic_arg("Opening ASCII output file \"%s\"...",filename.c_str());
+	fout.open(filename.c_str());
 	if(!fout.is_open()){
 		diagnostic("failed!\n");
 		exit(-1);	//TODO: Need to make this safer! Don't just close after all that work!
@@ -29,18 +34,49 @@ int output_ascii_data(const char filename[], const array2d<T> &output_grid, int 
 	diagnostic("succeeded.\n");
 */
 
-	diagnostic("Writing file header...");
-	fout<<"ncols\t\t"<<output_grid.width()<<std::endl;
-	fout<<"nrows\t\t"<<output_grid.height()<<std::endl;
-	fout<<"xllcorner\t"<<std::fixed<<std::setprecision(precision)<<output_grid.xllcorner<<std::endl;
-	fout<<"yllcorner\t"<<std::fixed<<std::setprecision(precision)<<output_grid.yllcorner<<std::endl;
-	fout<<"cellsize\t"<<std::fixed<<std::setprecision(precision)<<output_grid.cellsize<<std::endl;
-	fout<<"NODATA_value\t"<<std::fixed<<std::setprecision(precision)<<output_grid.no_data<<std::endl;
+	//OmniGlyph output
+	if(filename.substr(filename.length()-4)==".omg"){
+		outputsep="|";
+		output_type=OUTPUT_OMG;
+		diagnostic("Writing OmniGlyph file header...");
+		fout<<"Contents: Pixel array"<<std::endl;
+		fout<<std::endl;
+		fout<<"Width:    "<<output_grid.width()<<std::endl;
+		fout<<"Height:   "<<output_grid.height()<<std::endl;
+		fout<<std::endl;
+		fout<<"Spectral bands:   1"<<std::endl;
+		fout<<"Bits per band:   32"<<std::endl;
+		fout<<"Range of values:   "<<output_grid.min()<<","<<output_grid.max()<<std::endl;
+		fout<<"Actual range:   "<<output_grid.no_data<<","<<output_grid.max()<<std::endl;	//TODO: Assumes no_data is a small negative value
+		fout<<"Gamma exponent:   0."<<std::endl;
+		fout<<"Resolution:   100 pixels per inch"<<std::endl;
+		fout<<std::endl;
+		fout<<"|"<<std::endl;
+	} else {
+		diagnostic("Writing ArcGrid ASCII file header...");
+		fout<<"ncols\t\t"<<output_grid.width()<<std::endl;
+		fout<<"nrows\t\t"<<output_grid.height()<<std::endl;
+		fout<<"xllcorner\t"<<std::fixed<<std::setprecision(precision)<<output_grid.xllcorner<<std::endl;
+		fout<<"yllcorner\t"<<std::fixed<<std::setprecision(precision)<<output_grid.yllcorner<<std::endl;
+		fout<<"cellsize\t"<<std::fixed<<std::setprecision(precision)<<output_grid.cellsize<<std::endl;
+		fout<<"NODATA_value\t"<<std::fixed<<std::setprecision(precision)<<output_grid.no_data<<std::endl;
+	}
 	diagnostic("succeeded.\n");
 
-	diagnostic("Writing file data...");
-	fout<<std::setprecision(precision)<<output_grid;
-	diagnostic("succeeded.\n");
+	for(int y=0;y<output_grid.height();y++){
+		if(output_type==OUTPUT_OMG)
+			fout<<"|";
+		for(int x=0;x<output_grid.width();x++)
+			if(sizeof(T)==1)	//TODO: This is a crude way of detecting chars and bools
+				fout<<(int)output_grid(x,y)<<outputsep;
+			else
+				fout<<std::fixed<<std::setprecision(precision)<<output_grid(x,y)<<outputsep;
+		fout<<std::endl;
+	}
+
+//	diagnostic("Writing file data...");
+//	fout<<std::setprecision(precision)<<output_grid;
+//	diagnostic("succeeded.\n");
 
 	fout.close();
 
