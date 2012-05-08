9a815ec1908bdc113d259134d0f243eab15641c7
Richard Barnes <rbarnes@umn.edu>
2012-05-08 19:12:38 +0000

    This version of main is a candidate for use in the ArcGIS interface
    git-svn-id: file:///home/rick/.svn/richdem@226 c4c8cafc-911d-468f-9f86-3f4f8cd09a06
    
    Former-commit-id: e83fc50b46b328577e365fcbd085e18cb7d78f5c

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#define  RICHDEM_VERSION  "0r~219"
 #include "utility.h"
 #include "data_structures.h"
 #include "data_io.h"
@@ -7,31 +8,77 @@
 #include "interface.h"
 #include "flat_resolution.h"
 #include "watershed.h"
-//#include "visualize.h"
-#include "debug.h"
-#include <sys/time.h>
-#include <iostream>
-#include <iomanip>
-#include <map>
+//#include "debug.h"
+#include <string>
+
+#include "tclap/CmdLine.h"
 
 int main(int argc, char **argv){
+	TCLAP::CmdLine cmd("RichDEM is a suite of DEM analysis functions for determining hydrologic properties. It has been developed by Richard Barnes (rbarnes@umn.edu). Find RichDEM on the web at \"http://www.richdem.com\".", ' ', RICHDEM_VERSION);
+	TCLAP::SwitchArg cl_d8("8","d8","Use the D8 flow metric (Dinf is default)", cmd, false);
+	TCLAP::SwitchArg cl_fill_pits("p","pits","Perform pit-filling prior to other operations", cmd, false);
+	TCLAP::ValueArg<std::string> cl_output_pit_filled("l","pitfilled","Output pit-filled DEM - only applicable if -p is specified",false,"","file",cmd);
+	TCLAP::ValueArg<std::string> cl_output_unresolved_flowdirs("u","uflowdirs","Output flow directions without flat resolution",false,"","file",cmd);
+	TCLAP::ValueArg<std::string> cl_output_resolved_flowdirs("f","flowdirs","Output flow directions with flats resolved",false,"","file",cmd);
+	TCLAP::ValueArg<std::string> cl_output_flow_acculm("a","acculm","Output flow accumulation (aka: contributing area, upslope area)",false,"","file",cmd); //TODO: Are these really all equivalent?
+	TCLAP::UnlabeledValueArg<std::string> cl_inputDEM("inputDEM", "The DEM to be processed (must be in ArcGrid ASCII format)", true, "", "Input DEM", cmd);
+	try {
+		cmd.parse( argc, argv );
+	//	int bob = nameArg.getValue();
+	//	bool reverseName = reverseSwitch.getValue();
+	} catch (TCLAP::ArgException &e) {
+		std::cerr << "Command line argument (" << e.argId() << ") error: " << e.error() << std::endl;
+	}
+
 	float_2d elevations;
-	load_ascii_data(argv[1],elevations);
+	load_ascii_data(cl_inputDEM.getValue().c_str(),elevations);
 
-	barnes_flood(elevations);
+	if(cl_fill_pits.getValue()){
+		barnes_flood(elevations);
+		if(!cl_output_pit_filled.getValue().empty())
+			output_ascii_data(cl_output_pit_filled.getValue().c_str(),elevations);
+	}
 
-	float_2d flowdirs(elevations);
-	dinf_flow_directions(elevations,flowdirs);
 
-	int_2d flat_resolution_mask(elevations), groups;
-	resolve_flats_barnes(elevations,flowdirs,flat_resolution_mask,groups);
+	if(cl_d8.getValue()){	//TODO: D8 functions haven't been tested lately
+		char_2d flowdirs(elevations);
+		d8_flow_directions(elevations,flowdirs);
+		if(!cl_output_unresolved_flowdirs.getValue().empty())
+			output_ascii_data(cl_output_unresolved_flowdirs.getValue().c_str(),flowdirs);
 
-	dinf_flow_flats(flat_resolution_mask,groups,flowdirs);
+		{
+			int_2d flat_resolution_mask(elevations), groups;
+			resolve_flats_barnes(elevations,flowdirs,flat_resolution_mask,groups);
 
-	float_2d area(elevations);
-	dinf_upslope_area(flowdirs, area);
+			d8_flow_flats(flat_resolution_mask,groups,flowdirs);
+			if(!cl_output_resolved_flowdirs.getValue().empty())
+				output_ascii_data(cl_output_resolved_flowdirs.getValue().c_str(),flowdirs);
+		}
 
-	output_ascii_data(argv[2],area,8);
+		int_2d area(elevations);
+		d8_upslope_area(flowdirs, area);
+		if(!cl_output_flow_acculm.getValue().empty())
+			output_ascii_data(cl_output_flow_acculm.getValue().c_str(),area);
+	} else {
+		float_2d flowdirs(elevations);
+		dinf_flow_directions(elevations,flowdirs);
+		if(!cl_output_unresolved_flowdirs.getValue().empty())
+			output_ascii_data(cl_output_unresolved_flowdirs.getValue().c_str(),flowdirs);
+
+		{
+			int_2d flat_resolution_mask(elevations), groups;
+			resolve_flats_barnes(elevations,flowdirs,flat_resolution_mask,groups);
+
+			dinf_flow_flats(flat_resolution_mask,groups,flowdirs);
+			if(!cl_output_resolved_flowdirs.getValue().empty())
+				output_ascii_data(cl_output_resolved_flowdirs.getValue().c_str(),flowdirs);
+		}
+
+		float_2d area(elevations);
+		dinf_upslope_area(flowdirs, area);
+		if(!cl_output_flow_acculm.getValue().empty())
+			output_ascii_data(cl_output_flow_acculm.getValue().c_str(),area);
+	}
 
 	return 0;
 }
