47b84da248f5ff958c7c546a0f88cd66ae37c031
Richard Barnes <rbarnes@umn.edu>
2012-05-11 05:04:50 +0000

    ArcGIS progress support added
    git-svn-id: file:///home/rick/.svn/richdem@255 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- interface.cpp
+++ interface.cpp
@@ -34,14 +34,22 @@ double progress_bar(int percent){
 	if(percent==-1){
 		timediffs=timediff(startTime);
 		gettimeofday(&startTime, NULL);
+#ifndef ARCGIS
 		fprintf(stderr,"\r\033[2K"); //The ANSI code used in this line clears the entire line
+#else
+		fprintf(stderr,"P%%c\n"); //This is a control symbol the ArcGIS python script can search for to reset its progress bar
+#endif
 		old_percent=0;
 		return timediffs;
 	}
 	percent*=omp_get_num_threads();
 	if(percent>100)
 		percent=100;
+#ifndef ARCGIS
 	fprintf(stderr,"\r\033[2K[%-50.*s] (%d%% - %.1lfs left - %d thread)", percent/2, PROGRESS_BAR, percent, timediff(startTime)/percent*(100-percent),omp_get_num_threads()); //The ANSI code used in this line clears the entire line
+#else
+	fprintf(stderr,"P%%%d\n", percent); //This is a control symbol which the ArcGIS python script can search for to adjust its progress bar
+#endif
 	old_percent=percent;
 
 	return 0;
