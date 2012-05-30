727e8bc951f35892ef7489b15458ae8d8dc378bb
Richard Barnes <rbarnes@umn.edu>
2012-05-30 04:31:47 +0000

    Improvements and extensions to unit testing
    git-svn-id: file:///home/rick/.svn/richdem@309 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- unit_test.h
+++ unit_test.h
@@ -1,7 +1,8 @@
 #ifndef _unit_test_included
 #define _unit_test_included
 
-#include<cmath>
+#include <cmath>
+#include "interface.h"
 
 template <class T>
 double unit_avg_diff(const array2d<T> &arr1, const array2d<T> &arr2){
@@ -16,6 +17,7 @@ double unit_avg_diff(const array2d<T> &arr1, const array2d<T> &arr2){
 	for(int y=0;y<arr2.height();y++){
 		if(arr1(x,y)==arr1.no_data || arr2(x,y)==arr2.no_data)
 			continue;
+//		diagnostic_arg("Percent slopes. RichDEM: %f, ArcGIS: %f\n",arr1(x,y),arr2(x,y));
 		diff+=fabs(arr1(x,y)-arr2(x,y));
 		ccount++;
 	}
