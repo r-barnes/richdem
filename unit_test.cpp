b606cde1714ce2869a1c3bc04f5fb7dd50ccdfbb
Richard Barnes <rbarnes@umn.edu>
2012-05-30 04:31:47 +0000

    Improvements and extensions to unit testing
    git-svn-id: file:///home/rick/.svn/richdem@309 c4c8cafc-911d-468f-9f86-3f4f8cd09a06
    
    Former-commit-id: 81970196574cb65a2a933601eac31df1a01862bf

--- unit_test.cpp
+++ unit_test.cpp
@@ -21,6 +21,35 @@ int main(){
 		d8_slope(elevations, slope_percent, TATTRIB_SLOPE_PERCENT);
 		load_ascii_data("unit_test/bf03_slope_percent.txt", unit_slope_percent);
 		printf("Average SLOPE PERCENT difference: %lf\n",unit_avg_diff(slope_percent, unit_slope_percent));
+		slope_percent.clear();
+		unit_slope_percent.clear();
+	}
+
+	{
+		float_2d aspect, unit_aspect;
+		d8_aspect(elevations, aspect);
+		load_ascii_data("unit_test/bf03_aspect.txt", unit_aspect);
+		printf("Average ASPECT difference: %lf\n",unit_avg_diff(aspect, unit_aspect));
+		aspect.clear();
+		unit_aspect.clear();
+	}
+
+	{
+		float_2d planform_curvature, unit_planform_curvature;
+		d8_planform_curvature(elevations, planform_curvature);
+		load_ascii_data("unit_test/bf03_aspect.txt", unit_planform_curvature);
+		printf("Average PLANFORM CURVATURE difference: %lf\n",unit_avg_diff(planform_curvature, unit_planform_curvature));
+		planform_curvature.clear();
+		unit_planform_curvature.clear();
+	}
+
+	{
+		float_2d profile_curvature, unit_profile_curvature;
+		d8_aspect(elevations, profile_curvature);
+		load_ascii_data("unit_test/bf03_profile_curvature.txt", unit_profile_curvature);
+		printf("Average PROFILE CURVATURE difference: %lf\n",unit_avg_diff(profile_curvature, unit_profile_curvature));
+		profile_curvature.clear();
+		unit_profile_curvature.clear();
 	}
 	
 }
