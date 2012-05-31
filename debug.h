216b29916f371a3f68e5ba21c513b4e41aec5b15
Richard Barnes <rbarnes@umn.edu>
2012-05-31 16:01:05 +0000

    Added avg_diff() function, which mostly duplicates unit_avg_diff() from unit_test.h, but doesn't provide as much diagnostic output
    git-svn-id: file:///home/rick/.svn/richdem@328 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- debug.h
+++ debug.h
@@ -163,4 +163,27 @@ void print_and_highlight(const array2d<T> &dem, int xh, int yh) {
 	}
 }
 
+template <class T>
+double avg_diff(const array2d<T> &arr1, const array2d<T> &arr2){
+	diagnostic("Finding average difference between two arrays...");
+	if(arr1.width()!=arr2.width() || arr1.height()!=arr2.height())
+		diagnostic("failed! The arrays do not have the same dimensions!\n");
+
+	double diff=0;
+	int ccount=0;
+	#pragma omp parallel for collapse(2) reduction(+:diff) reduction(+:ccount)
+	for(int x=0;x<arr1.width();x++)
+	for(int y=0;y<arr2.height();y++){
+		if(arr1(x,y)==arr1.no_data || arr2(x,y)==arr2.no_data)
+			continue;
+		if(arr1(x,y)!=arr1(x,y) || arr2(x,y)!=arr2(x,y))	//If it's NaN
+			continue;
+		diff+=fabs(arr1(x,y)-arr2(x,y));
+		ccount++;
+	}
+	diagnostic("success!\n");
+
+	return (float)(diff/ccount);
+}
+
 #endif
