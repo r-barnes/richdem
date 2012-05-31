ed23bfb9753b28ee93783a8046e29c920b88e6cb
Richard Barnes <rbarnes@umn.edu>
2012-05-31 16:42:12 +0000

    Fixed syntax error
    git-svn-id: file:///home/rick/.svn/richdem@331 c4c8cafc-911d-468f-9f86-3f4f8cd09a06
    
    Former-commit-id: 540985889ea966b7e59a95a66a3ee990d2a16adb

--- d8_methods.h
+++ d8_methods.h
@@ -30,7 +30,7 @@ void d8_curvature(const float_2d &elevations, float_2d &curvatures);
 void d8_profile_curvature(const float_2d &elevations, float_2d &profile_curvatures);
 void d8_planform_curvature(const float_2d &elevations, float_2d &planform_curvatures);
 
-void find_watersheds(float_2d &elevations, int_2d &labels, bool alter_elevations=false){
+void find_watersheds(float_2d &elevations, int_2d &labels, bool alter_elevations=false);
 void watershed_area(const int_2d &labels);
 
 //234
