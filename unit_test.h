4f8f59d45db379d3ba7cccf01880ee078cc3d41d
Richard Barnes <rbarnes@umn.edu>
2012-06-01 05:13:53 +0000

    Added angular average difference function. Average difference functions now calculate RMS differences
    git-svn-id: file:///home/rick/.svn/richdem@341 c4c8cafc-911d-468f-9f86-3f4f8cd09a06
    
    Former-commit-id: c452219dcb6398bb533fddc5290272dc0362dda5

--- unit_test.h
+++ unit_test.h
@@ -3,6 +3,7 @@
 
 #include <cmath>
 #include "interface.h"
+#include "utility.h"
 
 template <class T>
 double unit_avg_diff(const array2d<T> &arr1, const array2d<T> &arr2){
@@ -18,16 +19,43 @@ double unit_avg_diff(const array2d<T> &arr1, const array2d<T> &arr2){
 	for(int y=0;y<arr2.height();y++){
 		if(arr1(x,y)==arr1.no_data || arr2(x,y)==arr2.no_data)
 			continue;
-		if(sample++<20)
-			diagnostic_arg("\tRichDEM: %f, ArcGIS: %f\n",arr1(x,y),arr2(x,y));
+		if(sample++<15)
+			diagnostic_arg("\tRichDEM: %f, ArcGIS: %f, Divided: %f\n",arr1(x,y),arr2(x,y),arr1(x,y)/arr2(x,y));
 		if(arr1(x,y)!=arr1(x,y) || arr2(x,y)!=arr2(x,y))	//If it's NaN
 			continue;
-		diff+=fabs(arr1(x,y)-arr2(x,y));
+		diff+=SQ(arr1(x,y)-arr2(x,y));
 		ccount++;
 	}
 	diagnostic("success!\n");
 
-	return (float)(diff/ccount);
+	return (float)sqrt(diff/ccount);
+}
+
+template <class T>
+double unit_ang_avg_diff(const array2d<T> &arr1, const array2d<T> &arr2){
+	diagnostic("Finding average angle difference between two arrays...");
+	if(arr1.width()!=arr2.width() || arr1.height()!=arr2.height())
+		diagnostic("failed! The arrays do not have the same dimensions!\n");
+
+	double diff=0;
+	int ccount=0;
+	int sample=0;
+	#pragma omp parallel for collapse(2) reduction(+:diff) reduction(+:ccount)
+	for(int x=0;x<arr1.width();x++)
+	for(int y=0;y<arr2.height();y++){
+		if(arr1(x,y)==arr1.no_data || arr2(x,y)==arr2.no_data)
+			continue;
+		if(arr1(x,y)!=arr1(x,y) || arr2(x,y)!=arr2(x,y))	//If it's NaN
+			continue;
+		double temp=angdiff_deg(arr1(x,y),arr2(x,y));
+		if(sample++<20 || temp>10)
+			diagnostic_arg("\tRichDEM: %f, ArcGIS: %f, Difference: %f\n",arr1(x,y),arr2(x,y),temp);
+		diff+=SQ(temp);
+		ccount++;
+	}
+	diagnostic("success!\n");
+
+	return (float)sqrt(diff/ccount);
 }
 
 #endif
