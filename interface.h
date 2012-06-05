c16a06d71e6eaffa8959972ca496eae4a1355d2d
Richard Barnes <rbarnes@umn.edu>
2012-06-05 19:50:35 +0000

    Removed progress_bar and timediff functions from interface.h header. Removed their code from interface.cpp, leaving it empty. Eliminated it.
    git-svn-id: file:///home/rick/.svn/richdem@361 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- interface.h
+++ interface.h
@@ -7,8 +7,6 @@
 
 #define diagnostic_arg(message,...) fprintf(stderr,message,__VA_ARGS__)
 #define diagnostic(message) fprintf(stderr,message)
-double progress_bar(int percent);
-double timediff(const timeval &startTime);
 
 #ifndef ARCGIS
 	#define SUCCEEDED_IN "\t\033[96msucceeded in %.2lfs.\033[39m\n"
