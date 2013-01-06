f5a7271b5f1860248ed36d670df33253edaf7988
Richard Barnes <rbarnes@umn.edu>
2013-01-06 15:28:18 +0000

    Eliminated
    git-svn-id: file:///home/rick/.svn/richdem@441 c4c8cafc-911d-468f-9f86-3f4f8cd09a06

--- extraneous/pit_fill.h
+++
@@ -1,1 +0,0 @@
-int pit_fill_yonghe2009(float_2d &elevations);
--- extraneous/when_int_and_float_become_different.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int i;
-	float f;
-	for(i=0,f=0;;i++,f++)
-		if(i!=f)
-			printf("%d %f\n",i,f);
-}
