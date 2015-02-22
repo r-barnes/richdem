d7e9d9e89ef177bc323dac4e98b942467a2f8fe9
Richard Barnes <rbarnes@umn.edu>
2015-02-21 18:22:11 -0600

    Made a makefile

---
+++ makefile
@@ -0,0 +1,2 @@
+main: main.cpp
+	mpic++ -o parallel_flowdirs.exe -O3 `gdal-config --cflags` `gdal-config --libs` main.cpp -lgdal --std=c++11 -Wall
--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ do
   for i in `seq 1 10`;
   do
     echo "Performing Test #$i with $((p + 1)) processors"
-    mpirun -n $((p + 1)) ./a.out "tests/testdem${i}.d8" &> "test_output$i.log"
+    mpirun -n $((p + 1)) ./parallel_flowdirs.exe "tests/testdem${i}.d8" &> "test_output$i.log"
     if [ -a tests/output${i}_0 ]
     then
       cat output*asc | diff -w tests/output${i}_0 -
