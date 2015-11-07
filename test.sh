8fa5f016733247b4a1f204a59534db93d558e460
Richard Barnes <rbarnes@umn.edu>
2015-11-07 09:19:55 -0800

    Reorganize tests and add echo

--- test.sh
+++ test.sh
@@ -1,23 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 mkdir /z/retain
 
 #Generate a reference
-time mpirun -n 4 ./parallel_pit_fill.exe one @offloadall         ~/projects/watershed/data/beauford.tif /z/singlecore-
+time mpirun -n 4 ./parallel_pit_fill.exe one @offloadall         ~/data/beauford.tif /z/singlecore-
 
 #Does block size make a difference?
-time mpirun -n 4 ./parallel_pit_fill.exe one @offloadall         ~/projects/watershed/data/beauford.tif /z/a500block-   -w 500 -h 500 
-time mpirun -n 4 ./parallel_pit_fill.exe one @offloadall         ~/projects/watershed/data/beauford.tif /z/b600block-   -w 600 -h 600 
+time mpirun -n 4 ./parallel_pit_fill.exe one @offloadall         ~/data/beauford.tif /z/a500block-   -w 500 -h 500 
 gdal_merge.py -o /z/500block.tif -of GTiff -n -9999 -a_nodata -9999 /z/a*tif
+
+time mpirun -n 4 ./parallel_pit_fill.exe one @offloadall         ~/data/beauford.tif /z/b600block-   -w 600 -h 600 
 gdal_merge.py -o /z/600block.tif -of GTiff -n -9999 -a_nodata -9999 /z/b*tif
 
 
 #Does offloading make a difference?
-time mpirun -n 50 ./parallel_pit_fill.exe one @retainall          ~/projects/watershed/data/beauford.tif /z/c500block-retain-  -w 500 -h 500 
-time mpirun -n 4  ./parallel_pit_fill.exe one /z/retain/500block- ~/projects/watershed/data/beauford.tif /z/d500block-save-    -w 500 -h 500 
-
+time mpirun -n 50 ./parallel_pit_fill.exe one @retainall          ~/data/beauford.tif /z/c500block-retain-  -w 500 -h 500 
 gdal_merge.py -o /z/500block-retain.tif -of GTiff -n -9999 -a_nodata -9999 /z/c500block-retain-beauford*tif
+
+time mpirun -n 4  ./parallel_pit_fill.exe one /z/retain/500block- ~/data/beauford.tif /z/d500block-save-    -w 500 -h 500 
 gdal_merge.py -o /z/500block-save.tif   -of GTiff -n -9999 -a_nodata -9999 /z/d500block-save-beauford*tif
 
+#Calculate differences
 gdal_calc.py --NoDataValue=-9999 -A /z/singlecore-beauford-0-fill.tif -B /z/500block.tif        --calc="A-B" --outfile=/z/500block-diff.tif
 gdal_calc.py --NoDataValue=-9999 -A /z/singlecore-beauford-0-fill.tif -B /z/600block.tif        --calc="A-B" --outfile=/z/600block-diff.tif
 gdal_calc.py --NoDataValue=-9999 -A /z/singlecore-beauford-0-fill.tif -B /z/500block-retain.tif --calc="A-B" --outfile=/z/500block-retain-diff.tif
@@ -28,6 +30,8 @@ gdalinfo -mm /z/600block-diff.tif        | grep Max
 gdalinfo -mm /z/500block-retain-diff.tif | grep Max
 gdalinfo -mm /z/500block-save-diff.tif   | grep Max
 
+
+#exit
 
 cd /z/
 
