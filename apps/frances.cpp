d4f2540158721cecdb039b4a82e94169999fe67e
Richard Barnes <rbarnes@umn.edu>
2016-01-15 10:34:30 -0800

    Accept zscale as input

--- apps/frances.cpp
+++ apps/frances.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "../libs/flats/flat_resolution_dinf.hpp"
 #include "../libs/depressions/priority_flood.hpp"
 #include "../libs/common/Array2D.hpp"
@@ -7,7 +8,7 @@
 #include "../libs/methods/dinf_methods.hpp"
 
 template<class T>
-int PerformAlgorithm(std::string filename, std::string output_prefix){
+int PerformAlgorithm(std::string filename, std::string output_prefix, float zscale){
 
   Array2D<T> elevations(filename,false);
 
@@ -27,7 +28,7 @@ int PerformAlgorithm(std::string filename, std::string output_prefix){
   flowdirs.saveGDAL(output_prefix+"-flowdirs.tif", filename, 0, 0);
 
   Array2D<float> slope;
-  d8_slope(elevations,slope,TATTRIB_SLOPE_RISERUN,1);
+  d8_slope(elevations,slope,TATTRIB_SLOPE_RISERUN,zscale);
 
   slope.saveGDAL(output_prefix+"-slope.tif",filename,0,0);
 
@@ -46,29 +47,31 @@ int PerformAlgorithm(std::string filename, std::string output_prefix){
 }
 
 int main(int argc, char **argv){
-  if(argc!=3){
-    std::cerr<<argv[0]<<" <INPUT> <OUTPUT_PREFIX>"<<std::endl;
+  if(argc!=4){
+    std::cerr<<argv[0]<<" <ZSCALE> <INPUT> <OUTPUT_PREFIX>"<<std::endl;
     return -1;
   }
 
-  switch(peekGDALType(argv[1])){
+  float zscale = std::strtof(argv[1],NULL);
+
+  switch(peekGDALType(argv[2])){
     case GDT_Unknown:
-      std::cerr<<"Unrecognised data type: "<<GDALGetDataTypeName(peekGDALType(argv[1]))<<std::endl;
+      std::cerr<<"Unrecognised data type: "<<GDALGetDataTypeName(peekGDALType(argv[2]))<<std::endl;
       return -1;
     case GDT_Byte:
-      return PerformAlgorithm<uint8_t >(argv[1],argv[2]);
+      return PerformAlgorithm<uint8_t >(argv[2],argv[3],zscale);
     case GDT_UInt16:
-      return PerformAlgorithm<uint16_t>(argv[1],argv[2]);
+      return PerformAlgorithm<uint16_t>(argv[2],argv[3],zscale);
     case GDT_Int16:
-      return PerformAlgorithm<int16_t >(argv[1],argv[2]);
+      return PerformAlgorithm<int16_t >(argv[2],argv[3],zscale);
     case GDT_UInt32:
-      return PerformAlgorithm<uint32_t>(argv[1],argv[2]);
+      return PerformAlgorithm<uint32_t>(argv[2],argv[3],zscale);
     case GDT_Int32:
-      return PerformAlgorithm<int32_t >(argv[1],argv[2]);
+      return PerformAlgorithm<int32_t >(argv[2],argv[3],zscale);
     case GDT_Float32:
-      return PerformAlgorithm<float   >(argv[1],argv[2]);
+      return PerformAlgorithm<float   >(argv[2],argv[3],zscale);
     case GDT_Float64:
-      return PerformAlgorithm<double  >(argv[1],argv[2]);
+      return PerformAlgorithm<double  >(argv[2],argv[3],zscale);
     case GDT_CInt16:
     case GDT_CInt32:
     case GDT_CFloat32:
@@ -76,7 +79,7 @@ int main(int argc, char **argv){
       std::cerr<<"Complex types are unsupported. Sorry!"<<std::endl;
       return -1;
     default:
-      std::cerr<<"Unrecognised data type: "<<GDALGetDataTypeName(peekGDALType(argv[1]))<<std::endl;
+      std::cerr<<"Unrecognised data type: "<<GDALGetDataTypeName(peekGDALType(argv[2]))<<std::endl;
       return -1;
   }
 
