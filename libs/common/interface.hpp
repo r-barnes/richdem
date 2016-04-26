d070fdefc3762ab9ef3ef4ea224b9ca6f12803f0
Richard Barnes <rbarnes@umn.edu>
2016-04-25 23:32:00 -0700

    Don't run the interface so much!

--- libs/common/interface.hpp
+++ libs/common/interface.hpp
@@ -21,51 +21,52 @@
 #endif
 
 class Timer{
-  private:
-    timeval start_time; ///<Last time the timer was started
-    double accumulated_time; ///<Accumulated running time since creation
-    bool running; ///<True when the timer is running
-    ///Number of seconds between two time objects
-    double timediff(timeval beginning, timeval end){
-      long seconds, useconds;
-      seconds  = end.tv_sec  - beginning.tv_sec;
-      useconds = end.tv_usec - beginning.tv_usec;
-      return seconds + useconds/1000000.0;
-    }
-  public:
-    Timer(){
-      accumulated_time=0;
-      running=false;
-    }
-    void start(){
-      if(running)
-        std::logic_error("Timer was already started!");
-      running=true;
-      gettimeofday(&start_time, NULL);
-    }
-    double stop(){
-      if(!running)
-        std::logic_error("Timer was already stopped!");
-      running=false;
-      timeval end_time;
-      gettimeofday(&end_time, NULL);
+ private:
+  timeval start_time;      ///<Last time the timer was started
+  double accumulated_time; ///<Accumulated running time since creation
+  bool running;            ///<True when the timer is running
 
-      accumulated_time+=timediff(start_time,end_time);
+  ///Number of seconds between two time objects
+  double timediff(timeval beginning, timeval end){
+    long seconds, useconds;
+    seconds  = end.tv_sec  - beginning.tv_sec;
+    useconds = end.tv_usec - beginning.tv_usec;
+    return seconds + useconds/1000000.0;
+  }
+ public:
+  Timer(){
+    accumulated_time = 0;
+    running          = false;
+  }
+  void start(){
+    if(running)
+      std::logic_error("Timer was already started!");
+    running=true;
+    gettimeofday(&start_time, NULL);
+  }
+  double stop(){
+    if(!running)
+      std::logic_error("Timer was already stopped!");
+    running=false;
+    timeval end_time;
+    gettimeofday(&end_time, NULL);
 
-      return accumulated_time;
-    }
-    double accumulated(){
-      if(running)
-        std::logic_error("Timer is still running!");
-      return accumulated_time;
-    }
-    double lap(){
-      if(!running)
-        std::logic_error("Timer was not started!");
-      timeval lap_time;
-      gettimeofday(&lap_time, NULL);
-      return timediff(start_time,lap_time);
-    }
+    accumulated_time += timediff(start_time,end_time);
+
+    return accumulated_time;
+  }
+  double accumulated(){
+    if(running)
+      std::logic_error("Timer is still running!");
+    return accumulated_time;
+  }
+  double lap(){
+    if(!running)
+      std::logic_error("Timer was not started!");
+    timeval lap_time;
+    gettimeofday(&lap_time, NULL);
+    return timediff(start_time,lap_time);
+  }
 };
 
 #define PROGRESS_BAR "=================================================="
@@ -73,19 +74,34 @@ class Timer{
 class ProgressBar{
   private:
     long total_work;
+    int call_count;
+    int old_call_count;
     int old_percent;
+    int call_diff;
     Timer timer;
   public:
     void start(long total_work0){
+      timer = Timer();
       timer.start();
-      total_work  = total_work0;
-      old_percent = 0;
+      total_work     = total_work0;
+      old_percent    = 0;
+      call_count     = 0;
+      old_call_count = 0;
+      call_diff      = total_work/200;
       //This ANSI code clears the entire line
       std::cerr<<"\r\033[2K"<<std::flush;
     }
     void update(long work_done){
+      #ifdef NOPROGRESS
+        return;
+      #endif
       if(omp_get_thread_num()!=0)
         return;
+      ++call_count;
+      if(call_count-old_call_count!=call_diff)
+        return;
+
+      old_call_count = call_count;
 
       int percent;
       percent=(int)(work_done*omp_get_num_threads()*100/total_work);
@@ -95,13 +111,16 @@ class ProgressBar{
         return;
       old_percent=percent;
 
-      std::cerr<<"\r\033[2K["
-               <<std::string(percent/2, '=')<<std::string(50-percent/2, ' ')
-               <<"] ("
-               <<percent<<"% - "
-               <<std::fixed<<std::setprecision(1)<<timer.lap()/percent*(100-percent)
-               <<"s - "
-               <<omp_get_num_threads()<< " threads)"<<std::flush;
+      if(total_work!=-1)
+        std::cerr<<"\r\033[2K["
+                 <<std::string(percent/2, '=')<<std::string(50-percent/2, ' ')
+                 <<"] ("
+                 <<percent<<"% - "
+                 <<std::fixed<<std::setprecision(1)<<timer.lap()/percent*(100-percent)
+                 <<"s - "
+                 <<omp_get_num_threads()<< " threads)"<<std::flush;
+      else
+        std::cerr<<"\r\033[2K"<<std::setw(12)<<work_done<<" cells completed."<<std::endl;
     }
     double stop(){
       //This ANSI code clears the entire line
