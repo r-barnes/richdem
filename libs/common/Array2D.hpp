5c00f17e8b1937842747ae9e8b7629014a81b196
Richard Barnes <rbarnes@umn.edu>
2016-07-17 13:53:45 -0600

    Updated printStamp() documentation

--- libs/common/Array2D.hpp
+++ libs/common/Array2D.hpp
@@ -1035,16 +1035,15 @@ class Array2D {
   /**
     @brief Output a square of cells useful for determining raster orientation.
 
-    This method prints out a square block of cells, none of which are NoData.
-    The block will have more than 'size' different values and be the closest
-    such block to the top, then left, of the raster.
+    This method prints out a square block of cells whose upper-left corner is
+    the (integer-division) center of the raster.
 
     Since algorithms may have to flip rasters horizontally or vertically before
     manipulating them, it is important that all algorithms work on data in the
     same orientation. This method, used in testing, helps a user ensure that 
     their algorithm is orientating data correctly.
 
-    @param[in]  size   Output stamp will be size x size with > size unique values
+    @param[in]  size   Output stamp will be size x size
   */
   void printStamp(size_t size) const {
     const int sx = width()/2;
