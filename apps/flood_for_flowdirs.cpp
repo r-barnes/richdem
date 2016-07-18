fc5f5266d6899700e703f1281c340af6b8ca5958
Richard Barnes <rbarnes@umn.edu>
2016-07-18 14:37:18 -0600

    Initial import

---
+++ apps/flood_for_flowdirs.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "../libs/depressions/priority_flood.hpp"
+#include "../libs/common/Array2D.hpp"
+#include "../libs/flats/flat_resolution.hpp"
+#include "../libs/methods/d8_methods.hpp"
+
+template<class T>
+int PerformAlgorithm(std::string filename, std::string output_prefix){
+
+  Array2D<T> elevations(filename,false);
+
+  improved_priority_flood(elevations);
+
+  Array2D<float> flowdirs;
+
+  barnes_flat_resolution_d8(elevations,flowdirs,false);
+
+  flowdirs.saveGDAL(output_prefix+"-flowdirs.tif", 0, 0);
+
+  return 0;
+}
+
+int main(int argc, char **argv){
+  if(argc!=3){
+    std::cerr<<argv[0]<<" <INPUT> <OUTPUT_PREFIX>"<<std::endl;
+    return -1;
+  }
+
+  switch(peekGDALType(argv[1])){
+    case GDT_Unknown:
+      std::cerr<<"Unrecognised data type: "<<GDALGetDataTypeName(peekGDALType(argv[1]))<<std::endl;
+      return -1;
+    case GDT_Byte:
+      return PerformAlgorithm<uint8_t >(argv[1],argv[2]);
+    case GDT_UInt16:
+      return PerformAlgorithm<uint16_t>(argv[1],argv[2]);
+    case GDT_Int16:
+      return PerformAlgorithm<int16_t >(argv[1],argv[2]);
+    case GDT_UInt32:
+      return PerformAlgorithm<uint32_t>(argv[1],argv[2]);
+    case GDT_Int32:
+      return PerformAlgorithm<int32_t >(argv[1],argv[2]);
+    case GDT_Float32:
+      return PerformAlgorithm<float   >(argv[1],argv[2]);
+    case GDT_Float64:
+      return PerformAlgorithm<double  >(argv[1],argv[2]);
+    case GDT_CInt16:
+    case GDT_CInt32:
+    case GDT_CFloat32:
+    case GDT_CFloat64:
+      std::cerr<<"Complex types are unsupported. Sorry!"<<std::endl;
+      return -1;
+    default:
+      std::cerr<<"Unrecognised data type: "<<GDALGetDataTypeName(peekGDALType(argv[1]))<<std::endl;
+      return -1;
+  }
+
+  return 0;
+}
