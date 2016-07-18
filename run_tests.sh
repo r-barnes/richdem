5919607c49e697f384c167e2f08c653debaea95e
Richard Barnes <rbarnes@umn.edu>
2016-07-18 14:15:15 -0600

    Added support for automatically generating test output

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,21 @@
 
 #set -x
 
-rm -f output*asc
+if [[ $1 = "test" ]]; then
+  echo "Running output test on '$2'"
+  if [ -a tests/dev/$2 ]
+  then
+    mpirun -n 2 ./parallel_d8_accum.exe one @evict "tests/dev/$2" /z/out-%n.tif -w 5 -h 5 &> "/z/test_output$2.log"
+    ./assemble_ascii.exe /z/out-%n.tif 2 2 > /z/out-combined
+    cat /z/out-combined
+    exit 0
+  else
+    echo "Test file not found!"
+    exit 1
+  fi
+fi
+
+
 for p in `seq 1 2`;
 do
   for i in `ls tests/dev/*d8 | sed 's/.*\///'`;
