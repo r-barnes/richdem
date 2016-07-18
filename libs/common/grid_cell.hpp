ee26265ab1a8043e75dd8b902446fa1346bf367b
Richard Barnes <rbarnes@umn.edu>
2016-07-17 23:43:43 -0600

    Added many more libraries. Modernized syntax of grid_cell.hpp

--- libs/common/grid_cell.hpp
+++ libs/common/grid_cell.hpp
@@ -13,55 +13,100 @@
 #include <cmath>
 
 /// Stores the (x,y) coordinates of a grid cell
-class grid_cell {
-  public:
-    int x; ///< Grid cell's x-coordinate
-    int y; ///< Grid cell's y-coordinate
-    /// Initiate the grid cell without coordinates; should generally be avoided
-    grid_cell(){}
-    /// Initiate the grid cell to the coordinates (x0,y0)
-    grid_cell(int x, int y):x(x),y(y){}
+class GridCell {
+ public:
+  int x; ///< Grid cell's x-coordinate
+  int y; ///< Grid cell's y-coordinate
+  /// Initiate the grid cell without coordinates; should generally be avoided
+  GridCell(){}
+  /// Initiate the grid cell to the coordinates (x0,y0)
+  GridCell(int x, int y):x(x),y(y){}
 };
 
 
 /// Stores the (x,y,z) coordinates of a grid cell; useful for priority sorting
 /// with \ref grid_cellz_compare.
+/// TODO: Specialize for integers
 template<class elev_t>
-class grid_cellz : public grid_cell {
-  public:
-    elev_t z;         ///< Grid cell's z-coordinate
-    grid_cellz(int x, int y, elev_t z): grid_cell(x,y), z(z) {}
-    grid_cellz(){}
-    bool operator< (const grid_cellz& a) const { return ( std::isnan(z) && !std::isnan(a.z)) || z< a.z; }
-    bool operator> (const grid_cellz& a) const { return (!std::isnan(z) &&  std::isnan(a.z)) || z> a.z; }
-    bool operator>=(const grid_cellz& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || (!std::isnan(z) &&  std::isnan(a.z)) || z>=a.z; }
-    bool operator<=(const grid_cellz& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || ( std::isnan(z) && !std::isnan(a.z)) || z<=a.z; }
-    bool operator==(const grid_cellz& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || z==a.z; }
-    bool operator!=(const grid_cellz& a) const { return !(std::isnan(z) &&  std::isnan(a.z)) && z!=a.z; }
+class GridCellZ : public GridCell {
+ public:
+  elev_t z;         ///< Grid cell's z-coordinate
+  GridCellZ(int x, int y, elev_t z): GridCell(x,y), z(z) {}
+  GridCellZ(){}
+  //TODO: Add tests of isnan
+  bool operator> (const GridCellZ<elev_t>& a) const { return z>a.z; }
 };
+
+
+
+
+
+template<>
+class GridCellZ<double> : public GridCell {
+ public:
+  double z;         ///< Grid cell's z-coordinate
+  GridCellZ(int x, int y, double z): GridCell(x,y), z(z) {}
+  GridCellZ(){}
+  //TODO: Add tests of isnan
+  bool operator< (const GridCellZ<double>& a) const { return ( std::isnan(z) && !std::isnan(a.z)) || z< a.z; }
+  bool operator> (const GridCellZ<double>& a) const { return (!std::isnan(z) &&  std::isnan(a.z)) || z> a.z; }
+  bool operator>=(const GridCellZ<double>& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || (!std::isnan(z) &&  std::isnan(a.z)) || z>=a.z; }
+  bool operator<=(const GridCellZ<double>& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || ( std::isnan(z) && !std::isnan(a.z)) || z<=a.z; }
+  bool operator==(const GridCellZ<double>& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || z==a.z; }
+  bool operator!=(const GridCellZ<double>& a) const { return !(std::isnan(z) &&  std::isnan(a.z)) && z!=a.z; }
+};
+
+
+template<>
+class GridCellZ<float>: public GridCell {
+ public:
+  float z;         ///< Grid cell's z-coordinate
+  GridCellZ(int x, int y, float z): GridCell(x,y), z(z) {}
+  GridCellZ(){}
+  //TODO: Add tests of isnan
+  bool operator< (const GridCellZ<float>& a) const { return ( std::isnan(z) && !std::isnan(a.z)) || z< a.z; }
+  bool operator> (const GridCellZ<float>& a) const { return (!std::isnan(z) &&  std::isnan(a.z)) || z> a.z; }
+  bool operator>=(const GridCellZ<float>& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || (!std::isnan(z) &&  std::isnan(a.z)) || z>=a.z; }
+  bool operator<=(const GridCellZ<float>& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || ( std::isnan(z) && !std::isnan(a.z)) || z<=a.z; }
+  bool operator==(const GridCellZ<float>& a) const { return ( std::isnan(z) &&  std::isnan(a.z)) || z==a.z; }
+  bool operator!=(const GridCellZ<float>& a) const { return !(std::isnan(z) &&  std::isnan(a.z)) && z!=a.z; }
+};
+
+
+
+
+
+
+
+
+
+
 
 
 
 /// Stores the (x,y,z) coordinates of a grid cell and a priority indicator k;
 /// used by grid_cellz_pq.
 template<class elev_t>
-class grid_cellzk : public grid_cellz<elev_t> {
+class grid_cellzk : public GridCellZ<elev_t> {
   public:
     int k;           ///< Used to store an integer to make sorting stable
-    grid_cellzk(int x, int y, elev_t z, int k): grid_cellz<elev_t>(x,y,z), k(k) {}
+    grid_cellzk(int x, int y, elev_t z, int k): GridCellZ<elev_t>(x,y,z), k(k) {}
     grid_cellzk(){}
-    bool operator< (const grid_cellzk<elev_t>& a) const { return grid_cellz<elev_t>::z< a.z || ( std::isnan(grid_cellz<elev_t>::z) && !std::isnan(a.z)) || (grid_cellz<elev_t>::z==a.z && k<a.k) || (std::isnan(grid_cellz<elev_t>::z) && std::isnan(a.z) && k<a.k); }
-    bool operator> (const grid_cellzk<elev_t>& a) const { return grid_cellz<elev_t>::z> a.z || (!std::isnan(grid_cellz<elev_t>::z) &&  std::isnan(a.z)) || (grid_cellz<elev_t>::z==a.z && k>a.k) || (std::isnan(grid_cellz<elev_t>::z) && std::isnan(a.z) && k>a.k); }
+    bool operator< (const grid_cellzk<elev_t>& a) const { return GridCellZ<elev_t>::z< a.z || ( std::isnan(GridCellZ<elev_t>::z) && !std::isnan(a.z)) || (GridCellZ<elev_t>::z==a.z && k<a.k) || (std::isnan(GridCellZ<elev_t>::z) && std::isnan(a.z) && k<a.k); }
+    bool operator> (const grid_cellzk<elev_t>& a) const { return GridCellZ<elev_t>::z> a.z || (!std::isnan(GridCellZ<elev_t>::z) &&  std::isnan(a.z)) || (GridCellZ<elev_t>::z==a.z && k>a.k) || (std::isnan(GridCellZ<elev_t>::z) && std::isnan(a.z) && k>a.k); }
 };
 
+
+
+
+
 ///A priority queue of grid_cells, sorted by ascending height
-template<class elev_t>
-class grid_cellz_pq : public std::priority_queue<grid_cellz<elev_t>, std::vector<grid_cellz<elev_t> >, std::greater<grid_cellz<elev_t> > > {
-  public:
-    void push_cell(int x, int y, elev_t z){
-      std::priority_queue<grid_cellz<elev_t>, std::vector<grid_cellz<elev_t> >, std::greater<grid_cellz<elev_t> > >::push(grid_cellz<elev_t>(x,y,z));
-    }
-};
+template<typename elev_t>
+using GridCellZ_pq = std::priority_queue<GridCellZ<elev_t>, std::vector<GridCellZ<elev_t> >, std::greater<GridCellZ<elev_t> > >;
+
+
+
+
 
 ///A priority queue of grid_cells, sorted by ascending height or, if heights
 ///are equal, by the order of insertion
@@ -71,7 +116,7 @@ class grid_cellzk_pq : public std::priority_queue<grid_cellzk<elev_t>, std::vect
     int count;
   public:
     grid_cellzk_pq() : count(0) {}
-    void push(const grid_cellz<elev_t> &a){
+    void push(const GridCellZ<elev_t> &a){
       std::priority_queue<grid_cellzk<elev_t>, std::vector<grid_cellzk<elev_t> >, std::greater<grid_cellzk<elev_t> > >::push(grid_cellzk<elev_t>(a.x,a.y,a.z,count++));
     }
     void push_cell(int x, int y, elev_t z){
---
+++ libs/common/memory.hpp
@@ -0,0 +1,41 @@
+#ifndef _memory_hpp_
+#define _memory_hpp_
+
+#include <fstream>
+#include <string>
+
+//http://stackoverflow.com/a/671389/752843
+void ProcessMemUsage(long &vmpeak, long &vmhwm){
+  #if defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+    vmpeak = 0;
+    vmhwm  = 0;
+
+    std::ifstream fin("/proc/self/status");
+    if(!fin.good())
+      return;
+    
+    while (!vmpeak || !vmhwm){
+      std::string line;
+
+      if(!getline(fin,line)){ //Check if we could still read file
+        return;
+      }
+
+      if(line.compare(0,7,"VmPeak:")==0){        //Peak virtual memory size
+        vmpeak = std::stoi(line.substr(7));
+      // } else if(line.compare(0,7,"VmSize:")==0){ //Virtual memory size
+      //   std::cerr<<"T: "<<line.substr(7,10)<<std::endl;
+      //   vmsize = std::stoi(line.substr(7,10));
+      // } else if(line.compare(0,6,"VmRSS:")==0){  //Resident set size
+      //   std::cerr<<"T: "<<line.substr(7,10)<<std::endl;
+      //   vmrss = std::stoi(line.substr(7,10));
+      } else if(line.compare(0,6,"VmHWM:")==0){  //Peak resident set size
+        vmhwm = std::stoi(line.substr(6));
+      }
+    }
+  #else
+    #pragma message("Cannot check memory statistics for this OS.")
+  #endif
+}
+
+#endif
