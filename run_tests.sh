1539e5d20deac6cbf53718250a60b9c31e56d3bb
Richard Barnes <rbarnes@umn.edu>
2016-07-17 18:34:54 -0600

    Add tests. Improve testing methodology.

--- run_tests.sh
+++ run_tests.sh
@@ -5,16 +5,17 @@
 rm -f output*asc
 for p in `seq 1 2`;
 do
-  for i in `seq 1 10`;
+  for i in `ls tests/dev/*d8 | sed 's/.*\///'`;
   do
-    echo "Performing Test #$i with $((p + 1)) processors"
-    mpirun -n $((p + 1)) ./parallel_d8_accum.exe one @evict "tests/dev/testdem${i}.d8" /z/out-%n.tif -w 5 -h 5 &> "test_output$i.log"
+    echo "Performing Test '$i' with $((p + 1)) processors"
+    mpirun -n $((p + 1)) ./parallel_d8_accum.exe one @evict "tests/dev/$i" /z/out-%n.tif -w 5 -h 5 &> "/z/test_output$i.log"
     ./assemble_ascii.exe /z/out-%n.tif 1 1 > /z/out-combined
-    if [ -a tests/dev/output${i} ]
+    name=$(echo ${i} | cut -f 1 -d '.')
+    if [ -a tests/dev/${name}.out ]
     then
-      diff -w tests/dev/output${i} /z/out-combined
+      diff -w tests/dev/${name}.out /z/out-combined
     else
-      echo "No good values against which to check Test #$i"
+      echo "No good values against which to check Test '$i'"
     fi
   done
 done
