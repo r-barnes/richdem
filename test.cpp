e9d1975f933d852ba36f2a7a2ca7afd88c84d933
Richard Barnes <rbarnes@umn.edu>
2016-07-19 11:31:13 -0600

    Modifications to update to new version of RichDEM

--- test.cpp
+++ test.cpp
@@ -1,4 +1,4 @@
-#include "Array2D.hpp"
+#include "richdem/common/Array2D.hpp"
 #include <iostream>
 #include <cassert>
 #include <iomanip>
@@ -54,10 +54,10 @@ void GridPerimToArray(const Array2D<T> &grid, std::vector<T> &vec){
   vec2copy = grid.getRowData(0);                         //Top
   vec.insert(vec.end(),vec2copy.begin(),vec2copy.end());
 
-  vec2copy = grid.getColData(grid.viewWidth()-1);        //Right
+  vec2copy = grid.getColData(grid.width()-1);        //Right
   vec.insert(vec.end(),vec2copy.begin()+1,vec2copy.end());
   
-  vec2copy = grid.getRowData(grid.viewHeight()-1);       //Bottom
+  vec2copy = grid.getRowData(grid.height()-1);       //Bottom
   vec.insert(vec.end(),vec2copy.begin(),vec2copy.end()-1);
   
   vec2copy = grid.getColData(0);                         //Left
@@ -66,17 +66,17 @@ void GridPerimToArray(const Array2D<T> &grid, std::vector<T> &vec){
 
 
 int main(){
-  Array2D<int> dem(13,17);
+  Array2D<int> dem(5,7);
   int val=0;
-  for(int y=0;y<dem.viewHeight();y++)
-  for(int x=0;x<dem.viewWidth();x++)
+  for(int y=0;y<dem.height();y++)
+  for(int x=0;x<dem.width();x++)
     dem(x,y)=val;
 
-  for(int y=0;y<dem.viewHeight();y++){
-    for(int x=0;x<dem.viewWidth();x++){
+  for(int y=0;y<dem.height();y++){
+    for(int x=0;x<dem.width();x++){
       dem(x,y) = -1;
-      if(dem.edge_grid(x,y)){
-        dem(x,y) = xyToSerial(x,y,dem.viewWidth(),dem.viewHeight());
+      if(dem.isEdgeCell(x,y)){
+        dem(x,y) = xyToSerial(x,y,dem.width(),dem.height());
         std::cout<<std::setw(3)<<dem(x,y);
       } else
         std::cout<<std::setw(3)<<" ";
@@ -86,34 +86,34 @@ int main(){
 
   std::vector<int> edges;
   GridPerimToArray(dem,edges);
-  for(int s=0;s<edges.size();s++)
+  for(size_t s=0;s<edges.size();s++)
     std::cout<<std::setw(3)<<s;
   std::cout<<std::endl;
   for(const auto &e: edges)
     std::cout<<std::setw(3)<<e;
   std::cout<<std::endl;
 
-  for(int s=0;s<edges.size();s++)
+  for(int s=0;s<(int)edges.size();s++)
     if(s!=edges[s])
       std::cout<<"GridPerim to Order issue s="<<s<<" does not map to edge "<<edges[s]<<std::endl;
 
   std::cerr<<"Testing that serialToXY can get back to the xyToSerial input."<<std::endl;
-  for(int y=0;y<dem.viewHeight();y++)
-  for(int x=0;x<dem.viewWidth();x++){
-    if(!dem.edge_grid(x,y))
+  for(int y=0;y<dem.height();y++)
+  for(int x=0;x<dem.width();x++){
+    if(!dem.isEdgeCell(x,y))
       continue;
     int xr,yr;
-    int s_good = xyToSerial(x,y,dem.viewWidth(),dem.viewHeight());
-    serialToXY(s_good,xr,yr,dem.viewWidth(),dem.viewHeight());
+    int s_good = xyToSerial(x,y,dem.width(),dem.height());
+    serialToXY(s_good,xr,yr,dem.width(),dem.height());
     if(xr!=x || yr!=y)
       std::cout<<"("<<x<<","<<y<<") with serial "<<s_good<<" sadly maps to ("<<xr<<","<<yr<<")"<<std::endl;
   }
 
   std::cerr<<"Testing that xyToSerial can get back to the serialToXY input."<<std::endl;
-  for(int s_good=0;s_good<edges.size();s_good++){
+  for(int s_good=0;s_good<(int)edges.size();s_good++){
     int xr,yr;
-    serialToXY(s_good,xr,yr,dem.viewWidth(),dem.viewHeight());
-    int s_to_test = xyToSerial(xr,yr,dem.viewWidth(),dem.viewHeight());
+    serialToXY(s_good,xr,yr,dem.width(),dem.height());
+    int s_to_test = xyToSerial(xr,yr,dem.width(),dem.height());
     if(s_to_test!=s_good)
       std::cout<<"Stest failed."<<std::endl;
   }
