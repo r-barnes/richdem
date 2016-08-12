9087b150501489bffa1431967600b30d61b4f9c4
Richard Barnes <rbarnes@umn.edu>
2016-08-12 17:46:42 -0600

    Flip appropriately in flood_for_flowdirs

--- apps/flood_for_flowdirs.cpp
+++ apps/flood_for_flowdirs.cpp
@@ -8,14 +8,47 @@
 
 template<class T>
 int PerformAlgorithm(std::string filename, std::string outputname){
+  bool flipH = false; //TODO
+  bool flipV = false;
+  std::string flip = "noflip";
+
+  if(flip=="fliph")
+    flipH=true;
+  else if(flip=="flipv")
+    flipV=true;
+  else if(flip=="fliphv")
+    flipH=flipV=true;
+  else if(flip!="noflip"){
+    std::cerr<<"Unrecognised flip directive."<<std::endl;
+    return -1;
+  }
 
   Array2D<T> elevations(filename,false);
 
+  //Flip tiles if the geotransform demands it
+  if( (elevations.geotransform[1]<0) ^ flipH)
+    elevations.flipHorz();
+  if( (elevations.geotransform[5]>0) ^ flipV)
+    elevations.flipVert();
+
+
   improved_priority_flood(elevations);
+  
+  elevations.printStamp(5);
 
   Array2D<uint8_t> flowdirs;
 
   barnes_flat_resolution_d8(elevations,flowdirs,false);
+
+  flowdirs.printStamp(5);
+
+  //Flip tiles if the geotransform demands it
+  if( (flowdirs.geotransform[1]<0) ^ flipH)
+    flowdirs.flipHorz();
+  if( (flowdirs.geotransform[5]>0) ^ flipV)
+    flowdirs.flipVert();
+
+  flowdirs.printStamp(5);
 
   flowdirs.saveGDAL(outputname, 0, 0);
 
