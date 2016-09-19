06d0bee89e562bef5a9d896f61d5ef543c1535aa
Richard Barnes <rbarnes@umn.edu>
2016-09-18 22:05:37 -0500

    Added terrain generator

---
+++ tests/terrain_gen/terrain_gen.cpp
@@ -0,0 +1,26 @@
+#include "PerlinNoise.h"
+#include "richdem/common/Array2D.hpp"
+#include "richdem/common/version.hpp"
+#include <iostream>
+
+int main(int argc, char **argv){
+  std::string analysis = PrintRichdemHeader(argc,argv);
+
+  if(argc!=2){
+    std::cerr<<"Syntax: "<<argv[0]<<" <Size>"<<std::endl;
+    return -1;
+  }
+
+  const int tsize = std::stoi(argv[1]);
+
+  PerlinNoise pn;
+
+  Array2D<float> terrain(tsize,tsize);
+  for(int y=0;y<tsize;y++)
+  for(int x=0;x<tsize;x++)
+    terrain(x,y) = pn.noise(10*x/(double)tsize,10*y/(double)tsize,0.8);
+
+  terrain.saveGDAL("/z/out.tif");
+
+  return 0;
+}
