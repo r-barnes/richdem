bb5490ae614eeb348ddceb544caa80a5843aa55e
Richard Barnes <rbarnes@umn.edu>
2017-08-26 18:20:37 -0700

    Add zscale to rd_surface_area

--- apps/rd_surface_area.cpp
+++ apps/rd_surface_area.cpp
@@ -8,11 +8,11 @@
 #include "richdem/common/Array2D.hpp"
 
 template<class T>
-int PerformAlgorithm(std::string analysis, Array2D<T> elevation){
+int PerformAlgorithm(const double zscale, std::string analysis, Array2D<T> elevation){
   elevation.loadData();
 
   const double sa_without_topo = elevation.numDataCells()*elevation.getCellArea();
-  const double sa_with_topo    = dem_surface_area(elevation);
+  const double sa_with_topo    = dem_surface_area(elevation, zscale);
   const double sa_diff         = sa_with_topo-sa_without_topo;
   const double sa_increase     = 100*(sa_with_topo-sa_without_topo)/sa_without_topo;
 
@@ -42,11 +42,12 @@ int PerformAlgorithm(std::string analysis, Array2D<T> elevation){
 int main(int argc, char **argv){
   std::string analysis = PrintRichdemHeader(argc,argv);
   
-  if(argc!=2){
+  if(argc!=3){
     std::cerr<<"Calculate the surface are of a DEM accounting for topography."<<std::endl;
-    std::cerr<<argv[0]<<" <Input>"<<std::endl;
+    std::cerr<<argv[0]<<" <Input> <z-scale>"<<std::endl;
+    std::cerr<<"\t<z-scale> - All elevations are multiplied by the z-scale prior to calculation."<<std::endl;
     return -1;
   }
 
-  return PerformAlgorithm(argv[1],analysis);
+  return PerformAlgorithm(argv[1],std::stod(argv[2]),analysis);
 }
