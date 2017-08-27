fffc0e872b008fa3c0e1270d1f3ac0e2f996877d
Richard Barnes <rbarnes@umn.edu>
2017-08-26 19:00:25 -0700

    Changes message from exception to warning

--- include/richdem/misc/misc_methods.hpp
+++ include/richdem/misc/misc_methods.hpp
@@ -143,8 +143,10 @@ double dem_surface_area(
   std::cerr<<"p Succeeded in = "<<progress.stop()<<" s"<<std::endl;
 
   const double dem_planar_area = elevations.numDataCells()*elevations.getCellArea();
-  if(area<dem_planar_area)
-    throw std::runtime_error("Topographic surface area ("+std::to_string(area)+") was smaller than planar surface area ("+std::to_string(dem_planar_area) +") - something must be wrong!");
+  if(area<dem_planar_area){
+    std::cerr<<"W Topographic surface area ("+std::to_string(area)+") < planar surface area ("+std::to_string(dem_planar_area) +")! Choosing planar area.";
+    return dem_planar_area;
+  }
 
   return area;
 }
