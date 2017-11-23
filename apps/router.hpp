aab1ab59f986bc1301e372e42ea1104460bbedeb
Richard Barnes <rbarnes@umn.edu>
2017-11-23 10:34:47 -0600

    Don't auto-load data in router

--- apps/router.hpp
+++ apps/router.hpp
@@ -8,31 +8,31 @@ template<typename... Args>
 int PerformAlgorithm(std::string inputfile, Args... args){
   switch(richdem::peekGDALType(inputfile)){
     case GDT_Byte:    {
-      richdem::Array2D<uint8_t> arr(inputfile,false,0,0,0,0,false);
+      richdem::Array2D<uint8_t> arr(inputfile,false,0,0,0,0,false,false);
       return PerformAlgorithm(args..., arr);
     }
     case GDT_UInt16:  {
-      richdem::Array2D<uint16_t> arr(inputfile,false,0,0,0,0,false);
+      richdem::Array2D<uint16_t> arr(inputfile,false,0,0,0,0,false,false);
       return PerformAlgorithm(args..., arr);
     }
     case GDT_Int16:   {
-      richdem::Array2D<int16_t> arr(inputfile,false,0,0,0,0,false);
+      richdem::Array2D<int16_t> arr(inputfile,false,0,0,0,0,false,false);
       return PerformAlgorithm(args..., arr);
     }
     case GDT_UInt32:  {
-      richdem::Array2D<uint32_t> arr(inputfile,false,0,0,0,0,false);
+      richdem::Array2D<uint32_t> arr(inputfile,false,0,0,0,0,false,false);
       return PerformAlgorithm(args..., arr);
     }
     case GDT_Int32:   {
-      richdem::Array2D<int32_t> arr(inputfile,false,0,0,0,0,false);
+      richdem::Array2D<int32_t> arr(inputfile,false,0,0,0,0,false,false);
       return PerformAlgorithm(args..., arr);
     }
     case GDT_Float32: {
-      richdem::Array2D<float> arr(inputfile,false,0,0,0,0,false);
+      richdem::Array2D<float> arr(inputfile,false,0,0,0,0,false,false);
       return PerformAlgorithm(args..., arr);
     }
     case GDT_Float64: {
-      richdem::Array2D<double> arr(inputfile,false,0,0,0,0,false);
+      richdem::Array2D<double> arr(inputfile,false,0,0,0,0,false,false);
       return PerformAlgorithm(args..., arr);
     }
     case GDT_CInt16:
