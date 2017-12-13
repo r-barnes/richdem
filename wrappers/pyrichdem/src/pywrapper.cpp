88e674576063848571b1beaf5ad9faec6a9b7b7d
Richard Barnes <rbarnes@umn.edu>
2017-12-13 06:39:37 -0800

    Use template function to load various Array2D's. This supersedes the Python script that previously built this code.

--- wrappers/pyrichdem/src/pywrapper.cpp
+++ wrappers/pyrichdem/src/pywrapper.cpp
@@ -22,11 +22,164 @@ using namespace richdem;
 //py::array_t<double, py::array::c_style | py::array::forcecast>
 //forcecast forces a conversion. We don't use it here in order to ensure that memory is not unnecessarily copied
 
+// #include "pybind11_array2d.hpp"
+
+template<class T>
+void TemplatedWrapper(py::module &m){
+  m.def("rdFillDepressions",     &Zhou2016<T>,                     "@@depressions/Zhou2016pf.hpp:Zhou2016@@"); //TODO
+  m.def("rdPFepsilon",           &priority_flood_epsilon<T>,       "Fill all depressions with epsilon."); //TODO
+
+  m.def("rdBreach",              &Lindsay2016<T>,                  "TODO");
+  
+  //m.def("rdBreach",              [](Array2D<T> &dem, const int mode, bool fill_depressions){&Lindsay2016<T>(dem,mode,fill_depressions);}, "TODO");
+
+  m.def("TA_SPI",                &TA_SPI<T, float, double>,       "TODO");         
+  m.def("TA_CTI",                &TA_CTI<T, float, double>,       "TODO");         
+  m.def("TA_slope_riserun",      &TA_slope_riserun     <T>,       "TODO");                   
+  m.def("TA_slope_percentage",   &TA_slope_percentage  <T>,       "TODO");                      
+  m.def("TA_slope_degrees",      &TA_slope_degrees     <T>,       "TODO");                   
+  m.def("TA_slope_radians",      &TA_slope_radians     <T>,       "TODO");                   
+  m.def("TA_aspect",             &TA_aspect            <T>,       "TODO");            
+  m.def("TA_curvature",          &TA_curvature         <T>,       "TODO");               
+  m.def("TA_planform_curvature", &TA_planform_curvature<T>,       "TODO");                        
+  m.def("TA_profile_curvature",  &TA_profile_curvature <T>,       "TODO");                       
+
+  m.def("FA_Tarboton",           &FA_Tarboton         <T,double>, "TODO");
+  m.def("FA_Holmgren",           &FA_Holmgren         <T,double>, "TODO");
+  m.def("FA_Quinn",              &FA_Quinn            <T,double>, "TODO");
+  m.def("FA_Freeman",            &FA_Freeman          <T,double>, "TODO");
+  m.def("FA_FairfieldLeymarie",  &FA_FairfieldLeymarie<T,double>, "TODO");
+  m.def("FA_Rho8",               &FA_Rho8             <T,double>, "TODO");
+  m.def("FA_D8",                 &FA_D8               <T,double>, "TODO");
+  m.def("FA_OCallaghan",         &FA_OCallaghan       <T,double>, "TODO");
+
+  //TODO
+  // m.def(
+  //   "WrapNumPy",
+  //   [](py::handle src){
+  //     if (!py::array_t<T>::check_(src))
+  //       throw std::runtime_error("WrapNumPy: Array check failed!");
+
+  //     auto buf = py::array_t<T, py::array::c_style | py::array::forcecast>::ensure(src);
+  //     if (!buf)
+  //       throw std::runtime_error("WrapNumPy: Could not generate a buffer!")
+
+  //     auto dims = buf.ndim();
+  //     if (dims != 2 )
+  //       throw std::runtime_error("WrapNumPy: NumPy array must have 2 dimensions.")
+
+  //     value = rd::Array2D<T>((T*) buf.data(),buf.shape()[1],buf.shape()[0]);
+
+  //     return true;
+  //   },
+  //   "TODO"
+  // );
+
+  py::class_<Array2D<T>>(m, "Array2D_" + TODO, py::buffer_protocol(), py::dynamic_attr())
+      .def(py::init<>())
+      .def(py::init<typename Array2D<T>::xy_t, typename Array2D<T>::xy_t,T>())
+      
+      .def(py::init<const Array2D<float   >&, T>())
+      .def(py::init<const Array2D<double  >&, T>())
+      .def(py::init<const Array2D<int8_t  >&, T>())
+      .def(py::init<const Array2D<int16_t >&, T>())
+      .def(py::init<const Array2D<int32_t >&, T>())
+      .def(py::init<const Array2D<int64_t >&, T>())
+      .def(py::init<const Array2D<uint8_t >&, T>())
+      .def(py::init<const Array2D<uint16_t>&, T>())
+      .def(py::init<const Array2D<uint32_t>&, T>())
+      .def(py::init<const Array2D<uint64_t>&, T>())
+
+      .def(py::init<T*, const int, const int>())
+      .def("size",      &Array2D<T>::size)
+      .def("width",     &Array2D<T>::width)
+      .def("height",    &Array2D<T>::height)
+      .def("empty",     &Array2D<T>::empty)
+      .def("noData",    &Array2D<T>::noData)
+      .def("min",       &Array2D<T>::min)
+      .def("max",       &Array2D<T>::max)
+      
+      //TODO: Simplify by casting to double in Python
+      .def("setNoData", [](Array2D<T> &a, const float    ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const double   ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const int8_t   ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const int16_t  ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const int32_t  ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const int64_t  ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const uint8_t  ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const uint16_t ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const uint32_t ndval){ a.setNoData((T)ndval); })
+      .def("setNoData", [](Array2D<T> &a, const uint64_t ndval){ a.setNoData((T)ndval); })
+            
+      .def_readwrite("geotransform", &Array2D<T>::geotransform)
+      .def_readwrite("projection",   &Array2D<T>::projection)
+      .def_readwrite("metadata",     &Array2D<T>::metadata)
+      .def("copy", [](const Array2D<T> a){
+        return a;
+      })
+      .def("fromArray", [](Array2D<T> &a, py::handle src){
+        // if(!py::array_t<T>::check_(src)) //TODO: What's this about?
+          // return false;
+
+        auto buf = py::array_t<T, py::array::c_style | py::array::forcecast>::ensure(src);
+        if (!buf)
+          throw std::runtime_error("Unable to convert array to RichDEM object!");
+
+        auto dims = buf.ndim();
+        if (dims != 2 )
+          throw std::runtime_error("Array must have two dimensions!");
+
+        a.clear();
+        a.resize(buf.shape()[1], buf.shape()[0]);
+        T* dat = (T*)buf.data();
+        for(typename Array2D<T>::i_t i=0;i<a.size();i++)
+          a(i) = dat[i];
+      })
+      .def_buffer([](Array2D<T> &arr) -> py::buffer_info {
+        return py::buffer_info(
+          arr.getData(),
+          sizeof(T),
+          py::format_descriptor<T>::format(),
+          2,                                           //Dimensions
+          {arr.height(), arr.width()},                 //Shape
+          {sizeof(T) * arr.width(), sizeof(T)} //Stride (in bytes)
+        );
+      })
+      .def("__repr__",
+        [](const Array2D<T> &a) {
+            return "<RichDEM array: type=T, width="+std::to_string(a.width())+", height="+std::to_string(a.height())+", owned="+std::to_string(a.owned())+">";
+        }
+      )
+      .def("__call__",
+        [](Array2D<T> &a, const int x, const int y) -> T& {
+          return a(x,y);
+        }
+      )
+      .def("__call__",
+        [](Array2D<T> &a, const int i) -> T& {
+          return a(i);
+        }
+      );      
+}
+
+
+
 PYBIND11_MODULE(_richdem, m) {
   m.doc() = "Internal library used by pyRichDEM for calculations";
 
   py::bind_vector<std::vector<double>>(m, "VecDouble");
   py::bind_map<std::map<std::string, std::string>>(m, "MapStringString");
+
+  TemplatedWrapper<float>(m);
+  TemplatedWrapper<double>(m);
+  TemplatedWrapper<int8_t>(m);
+  TemplatedWrapper<int16_t>(m);
+  TemplatedWrapper<int32_t>(m);
+  TemplatedWrapper<int64_t>(m);
+  TemplatedWrapper<uint8_t>(m);
+  TemplatedWrapper<uint16_t>(m);
+  TemplatedWrapper<uint32_t>(m);
+  TemplatedWrapper<uint64_t>(m);
 
   // m.def(
   //   "getBoundedScoresForGeoJSON",
