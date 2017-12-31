72162600b42bef74332082a9f36e6ca63cf60692
Richard Barnes <rbarnes@umn.edu>
2017-12-31 16:36:11 -0600

    Bumped version

--- wrappers/pyrichdem/setup.py
+++ wrappers/pyrichdem/setup.py
@@ -64,7 +64,7 @@ It can flood or breach depressions, as well as calculate flow accumulation, slop
 #TODO: https://packaging.python.org/tutorials/distributing-packages/#configuring-your-project
 setuptools.setup(
   name              = 'richdem',
-  version           = '0.0.8',
+  version           = '0.0.9',
   description       = 'High-Performance Terrain Analysis',
   long_description  = long_description,
   url               = 'https://github.com/r-barnes/richdem',
