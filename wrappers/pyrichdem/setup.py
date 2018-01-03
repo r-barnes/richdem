bbe160453f2d663adb7ad16762d4a7049566ef4f
Richard Barnes <rbarnes@umn.edu>
2018-01-03 00:45:16 -0600

    Eliminate non-ASCII character

--- wrappers/pyrichdem/setup.py
+++ wrappers/pyrichdem/setup.py
@@ -56,7 +56,7 @@ long_description = """RichDEM is a set of digital elevation model (DEM) hydrolog
 
 RichDEM uses parallel processing and state of the art algorithms to quickly process even very large DEMs.
 
-RichDEM offers a variety of flow metrics, such as D8 and D∞.
+RichDEM offers a variety of flow metrics, such as D8 and D-infinity.
 
 It can flood or breach depressions, as well as calculate flow accumulation, slopes, curvatures, &c."""
 
