75ac0bb792574996c8ea59f79bb95c3cef4fbdf0
Richard Barnes <rbarnes@umn.edu>
2018-05-31 18:45:06 -0600

    Deploy only manylinux wheels and source

--- .ci/travis/deploy_to_pypi.sh
+++ .ci/travis/deploy_to_pypi.sh
@@ -1,7 +1,8 @@
 # if [[ "$TRAVIS_TAG" == v* ]]; then
   echo "Testing PW=$bob"
   echo "Uploading to PyPI."
-  twine upload -u rbarnes -p$PYPI_PASSWORD wheelhouse/*
+  twine upload -u rbarnes -p$PYPI_PASSWORD wheelhouse/*manylinux*whl
+  twine upload -u rbarnes -p$PYPI_PASSWORD wheelhouse/*.tar.gz
   echo "Done."
   # if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   #   echo "Creating a source distribution."
