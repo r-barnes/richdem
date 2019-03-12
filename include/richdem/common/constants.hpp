7046729bf7d18e48c7fa5d4e2f65d13a1f16e865
Richard Barnes <rbarnes@umn.edu>
2019-03-12 12:13:31 -0700

    Added d4r and d8r constant arrays

--- include/richdem/common/constants.hpp
+++ include/richdem/common/constants.hpp
@@ -29,6 +29,10 @@
 
 namespace richdem {
 
+///sqrt(2), used to generate distances from a central cell to its neighbours
+const double SQRT2 = 1.414213562373095048801688724209698078569671875376948;
+
+
 //Constant used to hold D8 flow directions
 typedef uint8_t d8_flowdir_t;
 typedef int8_t  flowdir_t;
@@ -38,16 +42,18 @@ typedef int8_t  flowdir_t;
 //Facet                 0   1   2   3   4  5  6  7   8
 const int  dx[9]     = {0, -1, -1,  0,  1, 1, 1, 0, -1}; ///< x offsets of D8 neighbours, from a central cell
 const int  dy[9]     = {0,  0, -1, -1, -1, 0, 1, 1,  1}; ///< y offsets of D8 neighbours, from a central cell
+const double d8r[9]  = {0,1,SQRT2,1,SQRT2,1,SQRT2,1,SQRT2};
 const bool n_diag[9] = {0,  0,  1,  0,  1, 0, 1, 0,  1}; ///< True along diagonal directions, false along north, south, east, west
-const int D8_WEST  = 1;
-const int D8_NORTH = 3;
-const int D8_EAST  = 5;
-const int D8_SOUTH = 7;
+const int D8_WEST    = 1;
+const int D8_NORTH   = 3;
+const int D8_EAST    = 5;
+const int D8_SOUTH   = 7;
 
 const int *const d8x = dx;
 const int *const d8y = dy;
 const int d4x[5]   = {0, -1,  0, 1, 0}; ///< x offsets of D4 neighbours, from a central cell
 const int d4y[5]   = {0,  0, -1, 0, 1}; ///< y offsets of D4 neighbours, from a central cell
+const double d4r[5] = {0, 1, 1, 1, 1};
 const int D4_WEST  = 1;
 const int D4_NORTH = 2;
 const int D4_EAST  = 3;
@@ -59,9 +65,6 @@ const int D4_SOUTH = 4;
 const int d8_inverse[9] = {0,5,6,7,8,1,2,3,4};
 
 const int d4_inverse[5] = {0, 3, 4, 1, 2};
-
-///sqrt(2), used to generate distances from a central cell to its neighbours
-const double SQRT2 = 1.414213562373095048801688724209698078569671875376948;
 
 ///Distances from a central cell to each of its 8 neighbours
 const double dr[9] = {0,1,SQRT2,1,SQRT2,1,SQRT2,1,SQRT2}; //TODO: Check these for new D8 directions
