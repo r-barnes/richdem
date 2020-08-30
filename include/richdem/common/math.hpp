2594ede8e34fef0f422464d47388f2732d5efd49
Richard Barnes <rbarnes@umn.edu>
2020-08-30 13:12:40 -0600

    Added floating-point comparison helper functions

--- include/richdem/common/math.hpp
+++ include/richdem/common/math.hpp
@@ -1,12 +1,37 @@
-#ifndef _richdem_math_hpp_
-#define _richdem_math_hpp_
+#pragma once
 
 #include <cstdint>
 #include <cmath>
 
 //TODO: Shim to make MSVC compile
-int fpclassify(uint64_t arg) throw {
+int fpclassify(uint64_t arg) {
   return FP_NORMAL;
 }
 
-#endif
+namespace richdem {
+
+static const auto RICHDEM_FP_COMPARISON_ERROR = 1e-6;
+
+///////////////////////////////////
+//Floating-Point Comparisons
+///////////////////////////////////
+
+///Is a<b?
+template<class T>
+inline bool fp_less_than(const T &a, const T &b){
+  return a<b || std::abs(a-b)<RICHDEM_FP_COMPARISON_ERROR;
+}
+
+///Is a>b?
+template<class T>
+inline bool fp_greater_than(const T &a, const T &b){
+  return a>b || std::abs(a-b)<RICHDEM_FP_COMPARISON_ERROR;
+}
+
+///Does a==b?
+template<class T>
+inline bool fp_equal_to(const T &a, const T &b){
+  return std::abs(a-b)<RICHDEM_FP_COMPARISON_ERROR;
+}
+
+}
