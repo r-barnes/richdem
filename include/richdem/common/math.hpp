ba5b34f15bec870b3254da88647dca9426e3a748
Richard Barnes <rbarnes@umn.edu>
2020-09-12 16:19:13 -0600

    Have fp_le(), fp_ge(), and fp_eq() take doubles, this eliminates ambiguous templates

--- include/richdem/common/math.hpp
+++ include/richdem/common/math.hpp
@@ -16,21 +16,18 @@ static const auto RICHDEM_FP_COMPARISON_ERROR = 1e-6;
 //Floating-Point Comparisons
 ///////////////////////////////////
 
-///Is a<b?
-template<class T>
-inline bool fp_less_than(const T &a, const T &b){
+///Is a<=b?
+inline bool fp_le(const double &a, const double &b){
   return a<b || std::abs(a-b)<RICHDEM_FP_COMPARISON_ERROR;
 }
 
-///Is a>b?
-template<class T>
-inline bool fp_greater_than(const T &a, const T &b){
+///Is a>=b?
+inline bool fp_ge(const double &a, const double &b){
   return a>b || std::abs(a-b)<RICHDEM_FP_COMPARISON_ERROR;
 }
 
 ///Does a==b?
-template<class T>
-inline bool fp_equal_to(const T &a, const T &b){
+inline bool fp_eq(const double &a, const double &b){
   return std::abs(a-b)<RICHDEM_FP_COMPARISON_ERROR;
 }
 
